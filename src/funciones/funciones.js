89f13c65a8865da648adf7f734b683470cc92f4b
Reflex <microa4@gmail.com>
2020-04-22 20:44:33 +0200

    añadidas imagenes

---
+++ src/funciones/funciones.js
@@ -0,0 +1,17 @@
+var slideIndex = 1;
+showSlides();
+
+function showSlides() {
+    console.log('KKKKKK');
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
+};
