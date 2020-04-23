a4f0717fbb839b8cbcb62a4243aa8abdb8d7417f
Reflex <microa4@gmail.com>
2020-04-23 23:56:54 +0200

    menu despliega

--- src/funciones/menu.js
+++ src/funciones/menu.js
@@ -1,31 +1,111 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    
+function menu()
+{
+  console.log("77777777777777");
 
-console.log("KKKKKKKK")
+$(function() {
+  console.log("8888888888888888888");
+
+    var pull = $('#pull');
+
+    menu = $('.links');
+
+    menuHeight = menu.height();
 
 
 
-    if (!event.target.matches('.dropbtn')) {
-      console.log("aaaaaaaaaaaaaaaaaaaaaa")
+    $(pull).on('click', function(e) {
+      console.log("999999999");
 
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      
-      for (i = 0; i < dropdowns.length; i++) {
-       
+        e.preventDefault();
 
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          
-          openDropdown.classList.remove('show');
-        }
-      }
+        menu.slideToggle();
+
+    });
+
+});
+
+
+
+$(window).resize(function(){
+
+    var w = $(window).width();
+
+    if(w > 320 && menu.is(':hidden')) {
+
+        menu.removeAttr('style');
+
     }
-  }
+
+});
+
+
+
+}
+
+//----------------
+
+
+
+// SCRIPT DE desplegable
+
+
+
+function desplegable()
+
+{
+
+				
+
+	jQuery.fn.animateAuto = function(prop, speed, callback){
+
+    var elem, height, width;
+
+    return this.each(function(i, el){
+
+        el = jQuery(el), elem = el.clone().css({"height":"auto","width":"auto"}).appendTo("body");
+
+        height = elem.css("height"),
+
+        width = elem.css("width"),
+
+        elem.remove();
+
+        
+
+        if(prop === "height")
+
+            el.animate({"height":height}, speed, callback);
+
+        else if(prop === "width")
+
+            el.animate({"width":width}, speed, callback);  
+
+        else if(prop === "both")
+
+            el.animate({"width":width,"height":height}, speed, callback);
+
+    });  
+
+	}
+
+	$(window).ready(function(){
+
+		$('h2').click(function(){
+
+			if($(this).next().hasClass('desplegado')){
+
+				$(this).next().removeClass('desplegado').animate({height:0},500);
+
+			}else{
+
+				$(this).next().addClass('desplegado').animateAuto("height",500);
+
+			}
+
+		})
+
+	})
+
+}	
+
+//----------------
