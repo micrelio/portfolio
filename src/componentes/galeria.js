ed1239f4eaea512f97b24bd01133bb87c9980c0c
Reflex <microa4@gmail.com>
2020-04-25 02:02:22 +0200

    componentes añadidos

---
+++ src/componentes/galeria.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class miGaleria extends HTMLElement {
+    constructor () {
+      super();
+       }
+       connectedCallback (){
+           this.innerHTML =
+                `
+                <section class="primera">
+    <div class="">
+        <div >
+
+        </div>
+        
+        <button type="button" (click)="cambiar('todos'); reset()"> Todos </button>
+        <button type="button" (click)="cambiar('logos'); reset()"> Logos </button>
+        <button type="button" (click)="cambiar('publicidad'); reset()"> Publicidad </button>
+        <button type="button" (click)="cambiar('posters'); reset()"> Posters </button>
+        <button type="button" (click)="cambiar('postales'); reset()"> Postales </button>
+        <button type="button" (click)="cambiar('pngs'); reset()"> Pngs </button>
+    </div>
+    
+</section>
+
+<section class="segunda" [ngSwitch]="true">
+    <div *ngSwitchCase=" this.seleccion ==='todos'">
+        <app-repertorio [tipo]="seleccion" [logos]="logos" [publicidad]="publicidad" [posters]="posters" [postales]="postales" [pngs]="pngs"></app-repertorio>
+    </div>
+    <!-- "options === 'd' || options === 'e' || options === 'f'" -->
+    <div *ngSwitchCase="this.seleccion ==='logos'">
+        <app-logos></app-logos>
+        <!-- <app-slider [res]=0 [count]="0" [tipo]="seleccion" [logos]="logos" [publicidad]="publicidad" [posters]="posters" [postales]="postales" [pngs]="pngs"></app-slider> -->
+    </div>
+    <div *ngSwitchCase="this.seleccion === 'publicidad'">
+        <app-publicidad></app-publicidad>
+    </div>
+    <div *ngSwitchCase="this.seleccion === 'postales'">
+        <app-postales></app-postales>
+    </div>
+    <div *ngSwitchCase="this.seleccion === 'posters'">
+        <app-posters></app-posters>
+    </div>
+    <div *ngSwitchCase="this.seleccion === 'pngs'">
+        <app-pngs></app-pngs>
+    </div>
+</section>
+               
+                `;
+       }
+    }
+  //  window.customElements.define('mi-menu', miGaleria);
+  customElements.define('mi-galeria', miGaleria);
---
+++ src/funciones/galeria.js
@@ -0,0 +1,44 @@
+var pathi = 'src/images/logos/';
+var count = 0;
+var res = 0;
+var seleccion = 'todos';
+var logos = ["src/images/logos/igr.jpg",
+    "assets/images/logos/igre.png",
+    "assets/images/logos/john.png",];
+var posters = ["assets/images/posters/quiromasajista.jpg"];
+var publicidad = [ "assets/images/publi/igreCampaña2019.jpg",
+"assets/images/publi/igreAntoniaCampaña2019.jpg",
+"assets/images/publi/igreCampaña2019B.jpg",
+"assets/images/publi/unChalecoUnaVida.jpg",];
+var postales = ["assets/images/postales/postalIgre.jpg",
+"assets/images/postales/postalIgreCorreo.jpg",];
+var pngs = ["assets/images/png/antonia.png",
+"assets/images/png/banderaEuropaDesvanecida.png",
+"assets/images/png/banderaEsquina.png",
+"assets/images/png/europaRedondo.png",
+"assets/images/png/igreNavidad.png",];
+// @Input() sum = true;
+// inputValue = 0;
+
+
+
+function reset(){
+    console.log('WWWWWWWWWWWWWWWWWWW');
+    this.count = 0;
+
+}
+
+//timer para que cambie la imagen a la vez que se carga si no carga la foto y despues la cambia, retrasando la aparicion de alguna
+function cambiar(id) {
+    let h = id;
+    //  setTimeout(() => {
+    this.time(h);
+    // }, 3000);
+}
+function time(j){
+    this.seleccion = j;
+    console.log(this.seleccion);
+}
+
+
+
