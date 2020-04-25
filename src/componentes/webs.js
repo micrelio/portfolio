a80c22e72835940b203d4f95fc8e007f774eb621
Reflex <microa4@gmail.com>
2020-04-25 14:22:46 +0200

    webs añadido y funcionando

---
+++ src/componentes/webs.js
@@ -0,0 +1,46 @@
+class miWebs extends HTMLElement {
+    constructor () {
+      super();
+       }
+       connectedCallback (){
+           this.innerHTML =
+                `
+   
+                
+                <section class="web">
+    <div class="banner">
+        <div class="imagen">
+            <img src="../images/banners/primeros.jpg">
+        </div>
+        <div class="titulo">
+            <a target="blank" href=" http://primeroscoc.com/">
+                <h1>Primeros coc</h1>
+            </a>
+        </div>
+    </div>
+</section>
+
+
+
+<section class="web">
+
+    <div class="banner">
+        <div class="imagen">
+            <img src="../images/banners/primeros.jpg">
+        </div>
+
+        <div class="titulo">
+            <a target="blank" href="https://guerras-coc-online.000webhostapp.com/">
+                <h1>Guerras online</h1>
+            </a>
+        </div>
+    </div>
+    <!-- <div class="separador"></div> -->
+</section>
+
+
+               
+                `;
+       }
+    }
+  customElements.define('mi-webs', miWebs);
