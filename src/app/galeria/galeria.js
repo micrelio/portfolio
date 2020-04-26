a6d5ca0bf799c42630fcadd267baf68435c9c941
Reflex <microa4@gmail.com>
2020-04-26 05:33:58 +0200

    galeria funcionando

--- src/app/galeria/galeria.js
+++ src/app/galeria/galeria.js
@@ -1,14 +1,14 @@
 class miGaleria extends HTMLElement {
-    constructor () {
-      super();
-       }
-       connectedCallback (){
-           this.innerHTML =
-               `
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML =
+      `
             <section id="galeria" class="primera">
     <div class="">
                 
-        <button type="button" onclick="cambiar('logos'); reset()"> Todos </button>
+        <button type="button" onclick="cambiar('todos'); reset()"> Todos </button>
         <button type="button" onclick="cambiar('logos'); "> Logos </button>
         <button type="button" onclick="cambiar('publicidad'); reset()"> Publicidad </button>
         <button type="button" onclick="cambiar('posters'); reset()"> Posters </button>
@@ -23,113 +23,209 @@ class miGaleria extends HTMLElement {
 
                
                 `;
-       }
-    }
-  customElements.define('mi-galeria', miGaleria);
+  }
+}
+customElements.define('mi-galeria', miGaleria);
 
 
 
 
-  switch (render) {
-    case 'todos':
-        document.getElementById("galeria").innerHTML = "<mi-inicio></mi-inicio>";
-        break;
+switch (render) {
+  case 'todos':
+    document.getElementById("galeria").innerHTML = "<mi-inicio></mi-inicio>";
+    break;
+  case 'logos':
+    document.getElementById("galeria").innerHTML = "<mi-logos></mi-logos>";
+    break;
+  case 'publicidad':
+    document.getElementById("galeria").innerHTML = "<mi-galeria></mi-galeria>";
+    break;
+  case 'postales':
+    document.getElementById("galeria").innerHTML = "<mi-videos></mi-videos>";
+    break;
+  case 'posters':
+    document.getElementById("galeria").innerHTML = "<mi-curriculum></mi-curriculum>";
+    break;
+  case 'pngs':
+    document.getElementById("galeria").innerHTML = "<mi-contacto></mi-contacto>";
+    break;
+  default:
+}
+
+
+
+
+// function cambiar() {
+
+
+
+// var doubles = pngs.map(
+//   function(){
+//     var path= "../../"+pngs;
+//            var img=document.createElement('img'); 
+//            img.setAttribute("src",path);
+//            img.setAttribute("width","35");
+//            img.setAttribute("height","24");
+//   }
+// );
+// console.log()
+// }
+
+
+
+
+
+
+
+/////funciona 
+
+
+
+
+function cambiar(id) {
+
+  console.log(id)
+  switch (id) {
+
+
+
+
     case 'logos':
-      document.getElementById("galeria").innerHTML = "<mi-logos></mi-logos>";
+      for (let i = 0; i < logos.length; i++) {
+        console.log(logos.length)
+        console.log(logos[i])
+        var path = "../../" + logos[i];
+        var img = document.createElement('img');
+        img.setAttribute("src", path);
+        img.setAttribute("width", "100");
+        img.setAttribute("height", "");
+        document.getElementById("content").appendChild(img);
+      }
+      console.log('me cago en la puta')
       break;
-       case 'publicidad':
-            document.getElementById("galeria").innerHTML = "<mi-galeria></mi-galeria>";
+
+ case 'publicidad':
+      for (let i = 0; i < publicidad.length; i++) {
+        console.log(publicidad.length)
+        console.log(publicidad[i])
+        var path = "../../" + publicidad[i];
+        var img = document.createElement('img');
+        img.setAttribute("src", path);
+        img.setAttribute("width", "100");
+        img.setAttribute("height", "");
+        document.getElementById("content").appendChild(img);
+      }
+      console.log('me cago en la puta')
+      break;
+
+      case 'posters':
+        for (let i = 0; i < posters.length; i++) {
+          console.log(posters.length)
+          console.log(posters[i])
+          var path = "../../" + posters[i];
+          var img = document.createElement('img');
+          img.setAttribute("src", path);
+          img.setAttribute("width", "100");
+          img.setAttribute("height", "");
+          document.getElementById("content").appendChild(img);
+        }
+        console.log('me cago en la puta')
         break;
         case 'postales':
-          console.log('videos')
-      document.getElementById("galeria").innerHTML = "<mi-videos></mi-videos>";
-      break;
-      case 'posters':
-      document.getElementById("galeria").innerHTML = "<mi-curriculum></mi-curriculum>";
-      break;
-      case 'pngs':
-        document.getElementById("galeria").innerHTML = "<mi-contacto></mi-contacto>";
+          for (let i = 0; i < postales.length; i++) {
+            console.log(postales.length)
+            console.log(postales[i])
+            var path = "../../" + postales[i];
+            var img = document.createElement('img');
+            img.setAttribute("src", path);
+            img.setAttribute("width", "100");
+            img.setAttribute("height", "");
+            document.getElementById("content").appendChild(img);
+          }
+          console.log('me cago en la puta')
           break;
+
+          case 'pngs':
+            for (let i = 0; i < pngs.length; i++) {
+              console.log(pngs.length)
+              console.log(pngs[i])
+              var path = "../../" + pngs[i];
+              var img = document.createElement('img');
+              img.setAttribute("src", path);
+              img.setAttribute("width", "100");
+              img.setAttribute("height", "");
+              document.getElementById("content").appendChild(img);
+            }
+            console.log('me cago en la puta')
+            break;
+
+
+
     default:
-      }
+      break;
+  }
 
 
-      
 
-      
-     
-  
- function cambiar () { 
-
-  
-       for (let i = 0; i < pngs.length; i++) {
-              var path= "../../"+pngs[i];
-      var img=document.createElement('img'); 
-      img.setAttribute("src",path);
-      img.setAttribute("width","35");
-      img.setAttribute("height","24");
-      
-      document.getElementById("content").appendChild(img);
-      }
- }
+}
 
 
- 
- function reset(){
+
+function reset() {
   console.log('WWWWWWWWWWWWWWWWWWW');
   count = 0;
 
 }
 
 
-    //   document.body.onload = addElement;
-    //   function cambiar () { 
-    //     for (var indice in logos) {
-    //       console.log("En el índice '" + indice + "' hay este valor: " + logos[indice]);
-    //     // crea un nuevo div 
-    //     // y añade contenido 
-    //     var newDiv = document.createElement("div"); 
-    //     var newContent = document.getElementById("images").innerHTML =  "<img style='width:20%' src=../../"+ logos[indice]+">";
-    //     newDiv.appendChild(newContent); //añade texto al div creado. 
-      
-    //     // añade el elemento creado y su contenido al DOM 
-    //     var currentDiv = document.getElementById("div1"); 
-    //     document.body.insertBefore(newDiv, currentDiv); 
-    //   }
-    // }
+//   document.body.onload = addElement;
+//   function cambiar () { 
+//     for (var indice in logos) {
+//       console.log("En el índice '" + indice + "' hay este valor: " + logos[indice]);
+//     // crea un nuevo div 
+//     // y añade contenido 
+//     var newDiv = document.createElement("div"); 
+//     var newContent = document.getElementById("images").innerHTML =  "<img style='width:20%' src=../../"+ logos[indice]+">";
+//     newDiv.appendChild(newContent); //añade texto al div creado. 
 
-     // function cambiar(id) {
-        
-       
-       
-      //  for (let i = 0; i < pngs.length; i++) {
-      //    const element = pngs[i];
-      //    console.log( pngs[i])
-     //   }
+//     // añade el elemento creado y su contenido al DOM 
+//     var currentDiv = document.getElementById("div1"); 
+//     document.body.insertBefore(newDiv, currentDiv); 
+//   }
+// }
+
+// function cambiar(id) {
 
 
-      // for (var indice in logos) {
-        //   console.log("En el índice '" + indice + "' hay este valor: " + logos[indice]);
-           
-      //   console.log(logos.length)
-        //document.getElementById("images").innerHTML = "<div><img style='width:20%' src=../../"+ logos[indice]+">, <img style='width:20%' src=../../"+ logos[indice-1]+">,<img style='width:20%' src=../../"+ logos[indice-2]+"></div>";
-       // document.getElementById("images").innerHTML =  "<div><img style='width:20%' src=../../"+ logos[indice]+">, <img style='width:20%' src=../../"+ logos[indice-1]+">,<img style='width:20%' src=../../"+ logos[indice-2]+"></div>";
 
-        // "<div><img style='width:20%' src=../../"+ logos[indice]+"></div>"
+//  for (let i = 0; i < pngs.length; i++) {
+//    const element = pngs[i];
+//    console.log( pngs[i])
+//   }
 
-          
-         
-      //     console.log(logos[indice]);
-          // document.createElement(tagName, [options]);
-          //document.getElementById("images").innerHTML = "<div><img style='width:20%' src=../../"+ logos[indice]+"></div>";
-          //document.getElementById("images").innerHTML = "<mi-logos></mi-logos>";
-          //document.getElementById("images").innerHTML = "<mi-curriculum></mi-curriculum>";
-          //document.write("<img "style="whidth:50rem"+" src=../../"+ logos[indice]+">")
-         // document.write("<br>")
-     //   }
-     //  }
-       
-       
+
+// for (var indice in logos) {
+//   console.log("En el índice '" + indice + "' hay este valor: " + logos[indice]);
+
+//   console.log(logos.length)
+//document.getElementById("images").innerHTML = "<div><img style='width:20%' src=../../"+ logos[indice]+">, <img style='width:20%' src=../../"+ logos[indice-1]+">,<img style='width:20%' src=../../"+ logos[indice-2]+"></div>";
+// document.getElementById("images").innerHTML =  "<div><img style='width:20%' src=../../"+ logos[indice]+">, <img style='width:20%' src=../../"+ logos[indice-1]+">,<img style='width:20%' src=../../"+ logos[indice-2]+"></div>";
+
+// "<div><img style='width:20%' src=../../"+ logos[indice]+"></div>"
+
+
+
+//     console.log(logos[indice]);
+// document.createElement(tagName, [options]);
+//document.getElementById("images").innerHTML = "<div><img style='width:20%' src=../../"+ logos[indice]+"></div>";
+//document.getElementById("images").innerHTML = "<mi-logos></mi-logos>";
+//document.getElementById("images").innerHTML = "<mi-curriculum></mi-curriculum>";
+//document.write("<img "style="whidth:50rem"+" src=../../"+ logos[indice]+">")
+// document.write("<br>")
+//   }
+//  }
+
+
 
 
 
@@ -139,7 +235,7 @@ class miGaleria extends HTMLElement {
 //        <app-repertorio [tipo]="seleccion" [logos]="logos" [publicidad]="publicidad" [posters]="posters" [postales]="postales" [pngs]="pngs"></app-repertorio>
 //    </div>
 //    <!-- "options === 'd' || options === 'e' || options === 'f'" -->
-  
+
 //  </section>
 //    <div *ngSwitchCase="this.seleccion ==='logos'">
 //          <app-logos></app-logos>
@@ -157,3 +253,27 @@ class miGaleria extends HTMLElement {
 //      <div *ngSwitchCase="this.seleccion === 'pngs'">
 //          <app-pngs></app-pngs>
 //      </div>
+
+
+
+
+
+
+//****************************************//
+//********arrays**************************//
+
+var logos = ["images/logos/igr.jpg",
+  "images/logos/igre.png",
+  "/images/logos/john.png",];
+var posters = ["images/posters/quiromasajista.jpg"];
+var publicidad = ["images/publi/igreCampaña2019.jpg",
+  "images/publi/igreAntoniaCampaña2019.jpg",
+  "images/publi/igreCampaña2019B.jpg",
+  "images/publi/unChalecoUnaVida.jpg",];
+var postales = ["images/postales/postalIgre.jpg",
+  "images/postales/postalIgreCorreo.jpg",];
+var pngs = ["images/png/antonia.png",
+  "images/png/banderaEuropaDesvanecida.png",
+  "images/png/banderaEsquina.png",
+  "images/png/europaRedondo.png",
+  "images/png/igreNavidad.png",];
