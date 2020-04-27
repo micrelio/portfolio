c69bb763117b09ea79fc338721068073081ace3e
Reflex <microa4@gmail.com>
2020-04-27 23:22:50 +0200

    antes de experimentar funcionando 100%

--- src/app/galeria/galeria.js
+++ src/app/galeria/galeria.js
@@ -54,15 +54,125 @@ function cambiar(id) {
     default:
   }
 }
-/////funciona
-//  function maquetar() {
-function f() {
-  for (let i = 0; i < logos.length; i++) {
-    var path = "../../" + logos[i];
-    var img = document.createElement('img');
-    img.setAttribute("src", path);
-    img.setAttribute("width", "");
-    img.setAttribute("height", "500rem");
-    document.getElementById("logos").appendChild(img);
+
+
+var total = logos.length;
+
+
+
+function cargaLogos(e) {
+  // //console.log('total', total)
+   for (let index = 0; index < total; index++) {
+     let indice = logos[index];
+     //console.log('index', logos[index])
+     if (indice != e) {
+ 
+       activo = e;
+       count = index;
+       index = total;
+     }
+     activar(indice);
+    // miniaturas(indice);
+   }
+ }
+
+function activar(a) {
+
+  count = 0;
+  var path = "../../" + a;
+  var img = document.createElement('img');
+  img.setAttribute("src", path);
+  img.setAttribute("width", "");
+  img.setAttribute("height", "500rem");
+  img.setAttribute("id", "kk");
+  //console.log(img)
+  document.getElementById("activo").appendChild(img);
+
+
+
+  var img2 = document.createElement('img');
+  img2.setAttribute("src", path);
+  img2.setAttribute("width", "");
+  img2.setAttribute("height", "50rem");
+  img2.setAttribute("id", "min");
+  document.getElementById("mini").appendChild(img2);
+  //document.getElementById("activo").appendChild(img);
+
+}
+
+function cambio(a, b) {
+  //console.log('imagen1', document.getElementById('kk'))
+  var path = "../../" + a;
+  var img = document.createElement('img');
+  img.setAttribute("src", path);
+  img.setAttribute("width", "");
+  img.setAttribute("height", "500rem");
+  img.setAttribute("id", "kk");
+  //console.log('imagen2', img)
+  document.getElementById("activo").replaceChild(img, kk);
+
+  var img2 = document.createElement('img');
+  img2.setAttribute("src", path);
+  img2.setAttribute("width", "");
+  img2.setAttribute("height", "50rem");
+  img2.setAttribute("id", "min");
+  document.getElementById("mini").replaceChild(img2, min);
+
+}
+
+function sum() {
+    if (count >= total-1) {
+   retorno =logos[0]
+    count = 0;
+    cambio(retorno);
+  }
+  else {
+    count++;
+    nuevo = logos[count];
+    cambio(nuevo);
   }
 }
+
+function res() {
+  console.log('resta')
+  if (count <= 0) {
+    retorno =logos[total-1]
+    count = total-1;
+    cambio(retorno);
+  }
+  else {
+    count--;
+  nuevo = logos[count];
+  console.log(nuevo);
+
+    cambio(nuevo);
+  }
+}
+
+
+
+function reset() {
+  //console.log('reset')
+  count = 0;
+  //console.log(count)
+}
+
+function intervalo() {
+  let r = setInterval(() => {
+    sum();
+  }, 3000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/app/galeria/logos.js
+++ src/app/galeria/logos.js
@@ -11,7 +11,7 @@ class miLogos extends HTMLElement {
 
         <section  class="principal">
         <div  class="logos" >
-            <button onclick="sum()">-</button>
+            <button onclick="res()">-</button>
             <div id="activo" class="imgG" ></div>
             <button  onclick="sum()">+</button>
         </div>
@@ -50,25 +50,10 @@ customElements.define('mi-logos', miLogos);
 // }
 
 //var total=0;
-var final = logos.length ;
-var total = logos.length;
 
 
-function cargaLogos(e) {
- // //console.log('total', total)
-  for (let index = 0; index < total; index++) {
-    let indice = logos[index];
-    //console.log('index', logos[index])
-    if (indice != e) {
 
-      activo = e;
-      count = index;
-      index = total;
-    }
-    activar(indice);
-   // miniaturas(indice);
-  }
-}
+
 
 // function miniaturas(a) {
 //   //console.log()
@@ -79,100 +64,3 @@ function cargaLogos(e) {
 
 //   document.getElementById("mini").replaceChild(img);
 // }
-
-function activar(a) {
-
-  count = 0;
-  var path = "../../" + a;
-  var img = document.createElement('img');
-  img.setAttribute("src", path);
-  img.setAttribute("width", "");
-  img.setAttribute("height", "500rem");
-  img.setAttribute("id", "kk");
-  //console.log(img)
-  document.getElementById("activo").appendChild(img);
-
-
-
-  var img2 = document.createElement('img');
-  img2.setAttribute("src", path);
-  img2.setAttribute("width", "");
-  img2.setAttribute("height", "50rem");
-  img2.setAttribute("id", "min");
-  document.getElementById("mini").appendChild(img2);
-  //document.getElementById("activo").appendChild(img);
-
-}
-
-function cambio(a, b) {
-  //console.log('imagen1', document.getElementById('kk'))
-  var path = "../../" + a;
-  var img = document.createElement('img');
-  img.setAttribute("src", path);
-  img.setAttribute("width", "");
-  img.setAttribute("height", "500rem");
-  img.setAttribute("id", "kk");
-  //console.log('imagen2', img)
-  document.getElementById("activo").replaceChild(img, kk);
-
-  var img2 = document.createElement('img');
-  img2.setAttribute("src", path);
-  img2.setAttribute("width", "");
-  img2.setAttribute("height", "50rem");
-  img2.setAttribute("id", "min");
-  document.getElementById("mini").replaceChild(img2, min);
-
-}
-
-function sum() {
-    if (count >= final-1) {
-   retorno =logos[0]
-    count = 0;
-    cambio(retorno);
-  }
-  else {
-    count++;
-    nuevo = logos[count];
-    cambio(nuevo);
-  }
-}
-
-function res() {
-  console.log('resta')
-  if (count <= 0) {
-    count = final;
-  }
-  else {
-    count--;
-    nuevo = logos[count];
-    //cambio(nuevo);
-  }
-}
-
-
-
-function reset() {
-  //console.log('reset')
-  count = 0;
-  //console.log(count)
-}
-
-function intervalo() {
-  let r = setInterval(() => {
-    sum();
-  }, 300000);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
