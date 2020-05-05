f4718616c5b6a0912bb997dac1f3b1a8875b0693
Reflex <microa4@gmail.com>
2020-05-05 17:50:25 +0200

    punto criticoy muchos cambios a partir de aqui

--- src/app/galeria/visor.js
+++ src/app/galeria/visor.js
@@ -42,7 +42,7 @@ class miVisor extends HTMLElement {
                 `;
     // esto arranca automatico 
 
-    
+
 
 
 
@@ -130,24 +130,17 @@ function carga(e) {
 }
 function renderizar(vista) {
   count = 0;
-   total = vista.length;
-//----Leemos el array
-   for (let index = 0; index < total; index++) {
-     indice = vista[index];
-     siguiente = vista[index + 1];
+  total = vista.length;
+  //----Leemos el array
+  for (let index = 0; index < total; index++) {
+    indice = vista[index];
+    siguiente = vista[index + 1];
 
 
-    div = document.createElement('div');
-    
-    div.setAttribute("name", "");
-    div.setAttribute("width", "");
-    div.setAttribute("height", "");
-    div.setAttribute("id", "activo");
-    div.setAttribute("class", "activo");
+   
 
 
-
-    console.log('siguiente2',siguiente )
+    console.log('siguiente2', siguiente)
     // anterior = vista[total - 1];
     //    anterior2 = vista[total - 2];
     console.log(index);
@@ -173,38 +166,56 @@ function renderizar(vista) {
     activarMiniLaterales()
   }
 }
-function activar(a, b,  c, ) {
+function activar(a, b, c, ) {
   console.log('b2')
   //primero ponemos el contador a cero para empezar por el primer puesto del array
   var path = "../../" + a;
   var pathAnterior = "../../" + b;
   var pathSiguiente = "../../" + c;
- 
+
   //La img es igual a un elemento que creamos del tipo imagen
-  img = document.createElement('img');
-  imgA = document.createElement('img');
-  imgS = document.createElement('img');
+   img = document.createElement('img');
+   imgA = document.createElement('img');
+   imgS = document.createElement('img');
+
+
+   div = document.createElement('div');
+   divA = document.createElement('div');
+   divS = document.createElement('div');
+
   
  
 
 
 
 
-//-----------------------------------------------
+  //-----------------------------------------------
   img.setAttribute("src", path);
   img.setAttribute("width", "");
   img.setAttribute("height", "");
   img.setAttribute("id", "imagen");
+  img.setAttribute("class", "imagen");
+
+  div.setAttribute("id", "img");
+  div.setAttribute("class", "activo");
   //img.setAttribute("class", "object  move-right");
   imgS.setAttribute("src", pathSiguiente);
   imgS.setAttribute("width", "");
   imgS.setAttribute("height", "");
   imgS.setAttribute("id", "imagen");
- //---------------------------------------------- 
+  imgS.setAttribute("class", "imagen");
+
+  divS.setAttribute("id", "imgS");
+  divS.setAttribute("class", "activo");
+  //---------------------------------------------- 
   imgA.setAttribute("src", pathAnterior);
   imgA.setAttribute("width", "");
   imgA.setAttribute("height", "");
   imgA.setAttribute("id", "imagen");
+  imgA.setAttribute("class", "imagen");
+
+  divA.setAttribute("id", "imgA");
+  divA.setAttribute("class", "activo");
   //------------------------------------------------
 
 
@@ -212,12 +223,13 @@ function activar(a, b,  c, ) {
   //img.setAttribute("class", "object  move-right");
   //En el elemento llamado activo renderizamos la imagen
   console.log('img', imgS)
+  document.getElementById("visor").appendChild(divA);
+  document.getElementById("imgA").appendChild(imgA);
   document.getElementById("visor").appendChild(div);
- document.getElementById("activo").appendChild(imgA);
-  document.getElementById("visor").appendChild(div);
-  document.getElementById("activo").appendChild(img);
-  document.getElementById("visor").appendChild(div);
-  document.getElementById("activo").appendChild(imgS);
+  document.getElementById("img").appendChild(img);
+  
+  document.getElementById("visor").appendChild(divS);
+  document.getElementById("imgS").appendChild(imgS);
 
   var imagenPath = "../../" + a;
   var img1 = document.createElement('img');
@@ -225,7 +237,7 @@ function activar(a, b,  c, ) {
   img1.setAttribute("width", "");
   img1.setAttribute("height", "");
   img1.setAttribute("id", "miniImagen");
- // document.getElementById("cajonImagen").appendChild(img1);
+  // document.getElementById("cajonImagen").appendChild(img1);
 }
 function sum() {
   //console.log(i)
@@ -239,48 +251,59 @@ function sum() {
   }
   else {
     count++;
-    central = vista[count];
-    anterior = vista[count - 1];
-    siguiente = vista[count + 1];
+  //  central = vista[count];
+ //   anterior = vista[count - 1];
+  siguiente = vista[count + 1];
     // var techo = document.getElementById("divCentral");
     //  techo.removeChild(img);
-
-
+  
     //setTimeout(document.getElementById("divSiguiente2").appendChild(imgS2), 3000000)
-   // divAnterior.removeChild(imgA);
-   div.setAttribute("class", "object  move-right");
-
-   //divSiguiente.setAttribute("class", "object  move-right");
-
-  // divAnterior.setAttribute("class", "object  move-right");
-    //document.getElementById("divSiguiente").appendChild(imgs);
-
-console.log(imgA)
-    setTimeout(retraso, 3000);
-    function retraso(){  
-    console.log('retraso')
-  //  var t = document.getElementById("divAnterior");
-   //  t.removeChild(imgA);
-    //  divCentral.setAttribute("id", "");
-
-    //  divSiguiente.setAttribute("id", "");
-   
-      // var path2 = "../../" + siguiente;
-
-      // imgS.setAttribute("src", path2);
-      // imgS.setAttribute("width", "");
-      // imgS.setAttribute("height", "");
-      // imgS.setAttribute("id", "siguiente");
-      // document.getElementById("divSiguiente").appendChild(imgS);
-
-       // img.setAttribute("class", " ");
+    // divAnterior.removeChild(imgA);
+    divA.setAttribute("class", "activo object  move-right");
     
-       // imgS.setAttribute("class", " ");
-    
-  }
-   // document.getElementById("divSiguiente").appendChild(imgS);
+    div.setAttribute("class", "activo object  move-right");
+    divS.setAttribute("class", "activo object  move-right");
 
-   // imgS2.setAttribute("class", "object  move-right");
+    
+
+    console.log(imgA)
+ //   setTimeout(retraso, 3000);
+    function retraso() {
+      console.log('retraso')
+      //  var t = document.getElementById("divAnterior");
+      //  t.removeChild(imgA);
+      //  divCentral.setAttribute("id", "");
+
+      //  divSiguiente.setAttribute("id", "");
+      var  imgX = document.createElement('img');
+
+      var divX = document.createElement('div');
+  
+       var path = "../../" + siguiente;
+console.log('sigiente2', path)
+imgX.setAttribute("src", path);
+imgX.setAttribute("width", "");
+imgX.setAttribute("height", "");
+imgX.setAttribute("id", "imagen");
+imgX.setAttribute("class", "imagen");
+
+divX.setAttribute("id", "imgX");
+divX.setAttribute("class", "activo ");
+
+divX.setAttribute("class", "activo object  move-right");
+
+
+       document.getElementById("visor").appendChild(divX);
+       document.getElementById("imgX").appendChild(imgx);
+
+      // img.setAttribute("class", " ");
+
+      // imgS.setAttribute("class", " ");
+
+    }
+    // document.getElementById("divSiguiente").appendChild(imgS);
+
+    // imgS2.setAttribute("class", "object  move-right");
 
     //cambio(central, anterior, siguiente);
     //  activarMiniLaterales();
