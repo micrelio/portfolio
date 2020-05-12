937f7ab383e1ef14154ff1f691ef69eedfec394f
Reflex <microa4@gmail.com>
2020-05-12 04:54:09 +0200

    curricum formacion

--- src/app/curriculum/curriculum.js
+++ src/app/curriculum/curriculum.js
@@ -34,32 +34,80 @@ class miCurriculum extends HTMLElement {
 
 
   <section class="datos">
-  <div  class="datosPersonales">
-  
-      <p>2019
-          BootCamp fullstack developer, en Greekshubs Valencia (Valencia).</p>
-  
-      <p>2018
-          Grado superior ASIR (administración de sistemas informáticos en red) academia Tobalcaide, Quart de Poblet
-          (Valencia).</p>
-  
-      <p>2013-2017
-          Curso de inglés, instituto Rodrigo Botet, instituto Manises (Valencia).</p>
-  
-      <p>2008
-          Grado superior de Control y regulación de sistemas autocontrolados, instituto Salvador Gadea, Aldaya (Valencia).
-      </p>
-  
-      <p>2002
-          Curso de trabajos en alta tensión en COVER, Manises (Valencia).</p>
-  
-      <p>2000
-          Grado medio: Electricidad en Aldaya (Valencia).</p>
-  
-      <p>1997
-          FP 1electrónica, instituto Gonzalo Anaya, Xirivella (Valencia).</p>
 
-          </div>
+  <div class="experiencia">
+  <div class="logotipo">
+      <img id="" class="corporativo" src="../../assets/images/logoEmpresa/.jpg">
+  </div>
+  <div class="descripcion">
+      <p>2019</p>
+      <p>BootCamp fullstack developer, en Greekshubs Valencia (Valencia).</p>
+  </div>
+</div>
+
+
+<div class="experiencia">
+<div class="logotipo">
+    <img id="" class="corporativo" src="../../assets/images/logoEmpresa/.jpg">
+</div>
+<div class="descripcion">
+<p>2018</p>
+<p>Grado superior ASIR (administración de sistemas informáticos en red) academia Tobalcaide, Quart de Poblet (Valencia).</p>
+</div>
+</div>
+
+<div class="experiencia">
+<div class="logotipo">
+    <img id="" class="corporativo" src="../../assets/images/logoEmpresa/.jpg">
+</div>
+<div class="descripcion">
+<p>2013-2017</p>
+<p>Curso de inglés, instituto Rodrigo Botet, instituto Manises (Valencia).</p>
+</div>
+</div>
+
+<div class="experiencia">
+<div class="logotipo">
+    <img id="" class="corporativo" src="../../assets/images/logoEmpresa/.jpg">
+</div>
+<div class="descripcion">
+<p>2008</p>
+<p>Grado superior de Control y regulación de sistemas autocontrolados, instituto Salvador Gadea, Aldaya (Valencia).
+</p>
+</div>
+</div>
+
+<div class="experiencia">
+<div class="logotipo">
+    <img id="" class="corporativo" src="../../assets/images/logoEmpresa/cover.jpg">
+</div>
+<div class="descripcion">
+<p>2002</p>
+<p> Curso de trabajos en alta tensión en COVER, Manises (Valencia).</p>
+</div>
+</div>
+
+<div class="experiencia">
+<div class="logotipo">
+    <img id="" class="corporativo" src="../../assets/images/logoEmpresa/.jpg">
+</div>
+<div class="descripcion">
+<p>2000</p>
+<p> Grado medio: Electricidad en Aldaya (Valencia).</p>
+</div>
+</div>
+
+<div class="experiencia">
+<div class="logotipo">
+    <img id="" class="corporativo" src="../../assets/images/logoEmpresa/.jpg">
+</div>
+<div class="descripcion">
+<p>1997</p>
+<p>FP 1electrónica, instituto Gonzalo Anaya, Xirivella (Valencia).</p>
+</div>
+</div>
+
+  
   </section>
   
       <div class="titulo">
@@ -72,8 +120,8 @@ class miCurriculum extends HTMLElement {
               <img id="" class="corporativo" src="../../assets/images/logoEmpresa/yoigo.png">
           </div>
           <div class="descripcion">
-              <p>09-2018
-                  Yoigo: Teleoperador como atención al cliente.</p>
+              <p>2018</p>
+              <p>Yoigo: Teleoperador como atención al cliente.</p>
           </div>
       </div>
   
@@ -82,8 +130,8 @@ class miCurriculum extends HTMLElement {
               <img id="" class="corporativo" src="../../assets/images/logoEmpresa/quafis.jpg">
           </div>
           <div class="descripcion">
-              <p>03-2018/06-2018
-                  Quafis: Practicas grado superior ASIR, tareas como maquetación con WordPress DIVI, servicio técnico,
+              <p>2018</p>
+              <p>Quafis: Practicas grado superior ASIR, tareas como maquetación con WordPress DIVI, servicio técnico,
                   montaje y puesta a punto de PC, servicio técnico online con TeamViewer.
               </p>
           </div>
@@ -94,8 +142,8 @@ class miCurriculum extends HTMLElement {
               <img id="" class="corporativo" src="../../assets/images/logoEmpresa/frusa.jpg">
           </div>
           <div class="descripcion">
-              <p>12-2015/09-2016
-                  Frusa Coca Cola: Técnico de sistemas industriales.
+              <p>2016</p>
+              <p>Frusa Coca Cola: Técnico de sistemas industriales.
                   https://www.alimarket.es/alimentacion/empresa/209431
               </p>
           </div>
@@ -106,8 +154,8 @@ class miCurriculum extends HTMLElement {
               <img id="" class="corporativo" src="../../assets/images/logoEmpresa/trolli.jpg">
           </div>
           <div class="descripcion">
-              <p>01-2014/11-2015
-                  Trolli: Técnico de sistemas industriales, responsable de turno.
+              <p>2015</p>
+              <p>Trolli: Técnico de sistemas industriales, responsable de turno.
                   https://www.trolli.es/
               </p>
           </div>
@@ -118,8 +166,8 @@ class miCurriculum extends HTMLElement {
               <img id="" class="corporativo" src="../../assets/images/logoEmpresa/power.jpg">
           </div>
           <div class="descripcion">
-              <p>06-2013/09-2013
-                  Tour Electric: Revisión y mejoras en plantas fotovoltaicas, trabajos en inversores y centros de
+              <p>2013</p>
+              <p>Tour Electric: Revisión y mejoras en plantas fotovoltaicas, trabajos en inversores y centros de
                   transformación en Inglaterra.
               </p>
           </div>
@@ -130,10 +178,9 @@ class miCurriculum extends HTMLElement {
               <img id="" class="corporativo" src="../../assets/images/logoEmpresa/grupoSiro.jpg">
           </div>
           <div class="descripcion">
-              <p>01-2010/01-2013
-                  Grupo Siro: Técnico de sistemas industriales.
-                  https://www.cerealtosirofoods.com/es/
-              </p>
+              <p>2013</p>
+              <p>Grupo Siro: Técnico de sistemas industriales.
+                  https://www.cerealtosirofoods.com/es/</p>
           </div>
       </div>
   
@@ -142,8 +189,8 @@ class miCurriculum extends HTMLElement {
               <img id="" class="corporativo" src="../../assets/images/logoEmpresa/dafsa.jpg">
           </div>
           <div class="descripcion">
-              <p>09-2008/11-2009
-                  Dafsa: Técnico de sistemas industriales, responsable de turno.
+              <p>2009</p>
+              <p>Dafsa: Técnico de sistemas industriales, responsable de turno.
                   www.dafsa.es
               </p>
           </div>
@@ -154,10 +201,9 @@ class miCurriculum extends HTMLElement {
               <img id="" class="corporativo" src="../../assets/images/logoEmpresa/diselcom.jpg">
           </div>
           <div class="descripcion">
-              <p>06-2006/08-2008
-                  Diselcom: Montaje de cuadros electrónicos en época de estudios.
-                  http://www.diselcom.com/
-              </p>
+              <p>2008</p>
+              <p>Diselcom: Montaje de cuadros electrónicos en época de estudios.
+                  http://www.diselcom.com/</p>
           </div>
       </div>
   
@@ -167,10 +213,9 @@ class miCurriculum extends HTMLElement {
               <img id="" class="corporativo" src="../../assets/images/logoEmpresa/martinezLoriente.jpg">
           </div>
           <div class="descripcion">
-              <p>04-2003/09-2005
-                  Martínez Loriente: Ayudante técnico de sistemas.
-                  http://www.delisano.es/es/
-              </p>
+              <p>2005</p>
+              <p>Martínez Loriente: Ayudante técnico de sistemas.
+                  http://www.delisano.es/es/</p>
           </div>
       </div>
   
@@ -179,10 +224,9 @@ class miCurriculum extends HTMLElement {
               <img id="" class="corporativo" src="../../assets/images/logoEmpresa/cover.jpg">
           </div>
           <div class="descripcion">
-              <p>03-2002/9-2002
-                  Cover: Mantenimiento en centros de transformación y subestaciones industriales de alta y media tensión.
-                  https://www.coversa.com/
-              </p>
+              <p>2002</p>
+              <p>Cover: Mantenimiento en centros de transformación y subestaciones industriales de alta y media tensión.
+                  https://www.coversa.com/</p>
           </div>
       </div>
   
