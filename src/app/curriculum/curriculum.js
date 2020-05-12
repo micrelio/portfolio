42025c97b9a5184bfc9937cf6cd2a7bd8508173c
Reflex <microa4@gmail.com>
2020-05-12 02:06:01 +0200

    curriculum

--- src/app/curriculum/curriculum.js
+++ src/app/curriculum/curriculum.js
@@ -5,16 +5,159 @@ class miCurriculum extends HTMLElement {
   connectedCallback() {
     this.innerHTML =
       `
+
+
+
+  
+      
+      
 <section>
 
-  <div>
+
+
+  <div class="datosPersonales">
     <h>José Antonio López Baena</h>
     <p>Nacido el 27 de julio de 1982 en Valencia (España).<p>
     <p>Quart dePoblet 46930 (Valencia).<p>
     <p>Email:microa4@gmail.com<p>
   </div>
 
+  
+  </section>
+
+
+  <section>
   <div>
+  <h1>Experiencia laboral</h1>
+  </div>
+      <section class="empresa">
+      <div class="logotipo">
+        <img id="" class="corporativo"  src="../../assets/images/logoEmpresa/yoigo.png">
+      <div>
+      <div class="descripcion">
+      <p>09-2018
+      Yoigo: Teleoperador como atención al cliente.</p>
+    <div>
+    </section>
+
+          <section class="empresa">
+      <div class="logotipo">
+        <img id="" class="corporativo"  src="../../assets/images/logoEmpresa/quafis.jpg">
+      <div>
+      <div class="descripcion">
+      <p>03-2018/06-2018
+      Quafis: Practicas grado superior ASIR, tareas como maquetación con WordPress DIVI, servicio técnico, montaje y puesta a punto de PC, servicio técnico online con TeamViewer.
+      </p>
+  
+    <div>
+    </section>
+
+      <section class="empresa">
+      <div class="logotipo">
+        <img id="" class="corporativo"  src="../../assets/images/logoEmpresa/frusa.jpg">
+      <div>
+      <div class="descripcion">
+      <p>12-2015/09-2016 
+      Frusa Coca Cola: Técnico de sistemas industriales.
+      https://www.alimarket.es/alimentacion/empresa/209431
+      </p>
+    <div>
+    </section>
+
+      <section class="empresa">
+      <div class="logotipo">
+        <img id="" class="corporativo"  src="../../assets/images/logoEmpresa/trolli.jpg">
+      <div>
+      <div class="descripcion">
+      <p>01-2014/11-2015
+      Trolli: Técnico de sistemas industriales, responsable de turno.
+      https://www.trolli.es/
+      </p>
+    <div>
+    </section>
+
+      <section class="empresa">
+      <div class="logotipo">
+        <img id="" class="corporativo"  src="../../assets/images/logoEmpresa/power.jpg">
+      <div>
+      <div class="descripcion">
+      <p>06-2013/09-2013
+      Tour Electric: Revisión y mejoras en plantas fotovoltaicas, trabajos en inversores y centros de transformación en Inglaterra.
+      </p>
+    <div>
+    </section>
+
+      <section class="empresa">
+      <div class="logotipo">
+        <img id="" class="corporativo"  src="../../assets/images/logoEmpresa/grupoSiro.jpg">
+      <div>
+      <div class="descripcion">
+      
+      <p>01-2010/01-2013
+      Grupo Siro: Técnico de sistemas industriales.
+      https://www.cerealtosirofoods.com/es/
+      </p>
+    <div>
+    </section>
+
+          <section class="empresa">
+      <div class="logotipo">
+        <img id="" class="corporativo"  src="../../assets/images/logoEmpresa/dafsa.jpg">
+      <div>
+      <div class="descripcion">
+      <p>09-2008/11-2009
+      Dafsa: Técnico de sistemas industriales, responsable de turno.
+      www.dafsa.es
+      </p>
+    <div>
+    </section>
+
+      <section class="empresa">
+      <div class="logotipo">
+        <img id="" class="corporativo"  src="../../assets/images/logoEmpresa/diselcom.jpg">
+      <div>
+      <div class="descripcion">
+      <p>06-2006/08-2008
+      Diselcom: Montaje de cuadros electrónicos en época de estudios.
+      http://www.diselcom.com/
+      </p>
+    <div>
+    </section>
+    
+
+      <section class="empresa">
+      <div class="logotipo">
+        <img id="" class="corporativo"  src="../../assets/images/logoEmpresa/martinezLoriente.jpg">
+      <div>
+      <div class="descripcion">
+      <p>04-2003/09-2005
+      Martínez Loriente: Ayudante técnico de sistemas.
+      http://www.delisano.es/es/
+      </p>
+    <div>
+    </section>
+
+      <section class="empresa">
+      <div class="logotipo">
+        <img id="" class="corporativo"  src="../../assets/images/logoEmpresa/cover.jpg">
+      <div>
+      <div class="descripcion">
+            <p>03-2002/9-2002
+      Cover: Mantenimiento en centros de transformación y subestaciones industriales de alta y media tensión.
+      https://www.coversa.com/
+      </p>
+    <div>
+    </section>
+    
+
+
+
+
+
+
+
+
+  <section>
     <h>Formación académica</h>
 
     <p>2019
@@ -39,64 +182,7 @@ class miCurriculum extends HTMLElement {
     FP 1electrónica, instituto Gonzalo Anaya, Xirivella (Valencia).</p>
   </div>
   
-  <div>
-    <h>Experiencia laboral</h>
-    <p>09-2018
-    Yoigo: Tele operador como atención al cliente.
-    </p>
-
-    <p>03-2018/06-2018
-    Quafis: Practicas grado superior ASIR, tareas como maquetación con WordPress DIVI, servicio técnico, montaje y puesta a punto de PC, servicio técnico online con TeamViewer.
-    </p>
-
-    <p>12-2015/09-2016 
-    Frusa Coca Cola: Técnico de sistemas industriales.
-    https://www.alimarket.es/alimentacion/empresa/209431
-    </p>
-
-    <p>01-2014/11-2015
-    Trolli: Técnico de sistemas industriales, responsable de turno.
-    https://www.trolli.es/
-    </p>
-
-    <p>06-2013/09-2013
-    Tour Electric: Revisión y mejoras en plantas fotovoltaicas, trabajos en inversores y centros de transformación en Inglaterra.
-    </p>
-
-    <p>01-2010/01-2013
-    Grupo Siro: Técnico de sistemas industriales.
-    https://www.cerealtosirofoods.com/es/
-    </p>
-
-    <p>09-2008/11-2009
-    Dafsa: Técnico de sistemas industriales, responsable de turno.
-    www.dafsa.es
-    </p>
-
-    <p>06-2006/08-2008
-    Diselcom: Montaje de cuadros electrónicos en época de estudios.
-    http://www.diselcom.com/
-    </p>
-
-    <p>04-2003/09-2005
-    Martínez Loriente: Ayudante técnico de sistemas.
-    http://www.delisano.es/es/
-    </p>
-
-    <p>11-2002/03-2003
-    Electrovalencia: Instalaciones eléctricas.
-    </p>
-
-    <p>03-2002/9-2002
-    Cover: Mantenimiento en centros de transformación y subestaciones industriales de alta y media tensión.
-    https://www.coversa.com/
-    </p>
-
-    <p>09-2000/01-2002
-    Elecbel: Montaje de instalaciones eléctricas en cámaras frigoríficas.
-    http://www.elecbel.com/
-    </p>
-  </div>
+ 
   
   <div>
     <h>Datos de interés:</h>
