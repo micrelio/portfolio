632612227c9719f1ba55affb878991d8199a18d8
unknown <microa4@gmail.com>
2020-09-26 17:58:32 +0200

    acabado calsh of clans

--- src/app/webs/webs.js
+++ src/app/webs/webs.js
@@ -5,73 +5,43 @@ class miWebs extends HTMLElement {
     connectedCallback() {
         this.innerHTML =
             `
-            <section class="web">
+<section class="web">
     <div class="titanium">
-       
         <div class="titulo">
             <a target="blank" href=" https://titaniumgamers.com/">
                 <h1>Titanium Gamers</h1>
             </a>
-            </div>
-            <p>Creada con WordPress, tema DIVI para la comunidad propia de jugadores Titanium Gamers durante el confinamiernto 2020.</p>
-        
+        </div>
+        <p>Creada con WordPress, tema DIVI para la comunidad propia de jugadores Titanium Gamers durante el confinamiernto 2020.</p>
     </div>
-   
     <div class="tiendaTitanium">
-       
-    <div class="titulo">
-        <a target="blank" href="https://titaniumgamers.com/tienda/samp/">
-            <h1>Tienda Titanium Gamers</h1>
-        </a>
+        <div class="titulo">
+            <a target="blank" href="https://titaniumgamers.com/tienda/samp/">
+                <h1>Tienda Titanium Gamers</h1>
+            </a>
         </div>
         <p>Creada con código por medio de una plantilla Bootstrap, para la comunidad de jugadores Titanium Gamers durante el confinamiernto 2020.</p>
     </div>
-   
-    <div class="tiendaTitanium">
-       
-    <div class="titulo">
-        <a target="blank" href="https://titaniumgamers.com/tienda/samp/">
-            <h1>Tienda Titanium Gamers</h1>
-        </a>
+    <div class="primeroscoc">
+        <div class="titulo">
+            <a target="blank" href="https://titaniumgamers.com/tienda/samp/">
+                <h1>Primeros Clash of Clans</h1>
+            </a>
         </div>
         <p>Creada con código por medio de una plantilla Bootstrap, para la comunidad de jugadores Titanium Gamers durante el confinamiernto 2020.</p>
     </div>
+    <div class="primeroscoc">
+        <div class="titulo">
+            <a target="blank" href="https://titaniumgamers.com/tienda/samp/">
+                <h1>Guerras onLine</h1>
+                <h1>Clash of Clans</h1>
+            </a>
+        </div>
+        <p>Creada con código por medio de una plantilla Bootstrap, para la comunidad de jugadores Titanium Gamers durante el confinamiernto 2020.</p>
+    </div>
+</section>
 
-    <div class="tiendaTitanium">
-       
-    <div class="titulo">
-        <a target="blank" href="https://titaniumgamers.com/tienda/samp/">
-            <h1>Tienda Titanium Gamers</h1>
-        </a>
-        </div>
-        <p>Creada con código por medio de una plantilla Bootstrap, para la comunidad de jugadores Titanium Gamers durante el confinamiernto 2020.</p>
-    </div>
-</section>
-<section class="web">
-    <div class="banner">
-        <div class="imagen">
-            <!-- <img src="../../assets/images/banners/primeros.jpg"> -->
-        </div>
-        <div class="titulo">
-            <a target="blank" href=" http://primeroscoc.com/">
-                <h1>Primeros coc</h1>
-            </a>
-        </div>
-    </div>
-</section>
-<section class="web">
-    <div class="banner">
-        <div class="imagen">
-         <!--   <img src="../../assets/images/banners/primeros.jpg"> -->
-       </div>
-        <div class="titulo">
-            <a target="blank" href="https://guerras-coc-online.000webhostapp.com/">
-                <h1>Guerras online</h1>
-            </a>
-        </div>
-    </div>
-    <!-- <div class="separador"></div> -->
-</section>
+
          `;
     }
 }
