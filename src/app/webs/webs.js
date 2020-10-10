a47347fbd6f40473f7f950d8327ee438bd9ecde5
unknown <microa4@gmail.com>
2020-10-11 00:35:47 +0200

    estilos mas

--- src/app/webs/webs.js
+++ src/app/webs/webs.js
@@ -15,12 +15,10 @@ class miWebs extends HTMLElement {
         <div>
             <p>Diseñada con WordPress tema DIVI (adquirida licencia desarrollador), para la comunidad propia de jugadores <a target="blank" href="https://titaniumgamers.com/">Titanium Gamers</a> durante el confinamiento 2020.<br>
             Creada con:<br>
-            <i class="fa fa-check-circle verde"></i> WORDPRESS tema DIVI.
-            </p>
-            <p>
-                <a class="visitar" target="blank" href="https://titaniumgamers.com/">Visitar
+            <i class="fa fa-check-circle verde"></i> WORDPRESS tema DIVI.<br><br>
+            <a class="visitar" target="blank" href="https://titaniumgamers.com/">Visitar
                 <i class="fas fa-arrow-alt-circle-right"></i></a>
-            </p><br>
+            </p>
         </div>
     </div>
 
@@ -33,17 +31,16 @@ class miWebs extends HTMLElement {
         </div>
         <p>Tienda de donaciones PayPal creada durante el verano 2020 para la comunidad propia de jugadores <a target="blank" href="https://tiendasamp.titaniumgamers.com/">Titanium Gamers</a>.<br>
         Estilos usados:<br>
-        <i class="fa fa-check-circle verde">
-        </i> Bootstrap </p>
-        <p>Lenguajes usados:<br>
-            <i class="fa fa-check-circle verde"></i> HTML<br>
-            <i class="fa fa-check-circle verde"></i> CSS<br>
-            <i class="fa fa-check-circle verde"></i> JAVASCRIPT
-        </p>
-        <p>
+        <i class="fa fa-check-circle verde"></i> Bootstrap.<br>
+        <i class="fa fa-check-circle verde"></i> Display Grid.<br>
+        <i class="fa fa-check-circle verde"></i> Display Flex.<br>
+        Lenguajes usados:<br>
+            <i class="fa fa-check-circle verde"></i> HTML.<br>
+            <i class="fa fa-check-circle verde"></i> CSS.<br>
+            <i class="fa fa-check-circle verde"></i> Javascript.<br><br>
             <a class="visitar" target="blank" href="https://tiendasamp.titaniumgamers.com/">Visitar
             <i class="fas fa-arrow-alt-circle-right"></i></a>
-        </p><br>
+        </p>
     </div>
     
     <div class="primeroscoc">
@@ -55,16 +52,14 @@ class miWebs extends HTMLElement {
         </div>
         <p>
             Diseñada como proyecto fin de curso ASIR durante el curso 2017-2018, a la comunidad de gamers <a target="blank" href="https://primeroscoc.titaniumgamers.com/">&#xf006;Primeros&#xf006;</a> jugadores de Clash of Clans.<br>
-            API que sincroniza con el servidor y muestra los datos de los equipos en tiempo real<br>
+            Esta API sincroniza con el servidor y muestra los datos de los equipos en tiempo real.<br>
             Lenguajes usados:<br>
-            <i class="fa fa-check-circle verde"></i> HTML<br>
-            <i class="fa fa-check-circle verde"></i> CSS<br>
-            <i class="fa fa-check-circle verde"></i> PHP
-        </p>
-        <p>
+            <i class="fa fa-check-circle verde"></i> HTML.<br>
+            <i class="fa fa-check-circle verde"></i> CSS.<br>
+            <i class="fa fa-check-circle verde"></i> PHP.<br><br>
             <a  class="visitar" target="blank" href="https://primeroscoc.titaniumgamers.com/">Visitar
             <i class="fas fa-arrow-alt-circle-right"></i></a>
-        </p><br>
+        </p>
     </div>
 
     <div class="guerrasonline">
@@ -78,14 +73,12 @@ class miWebs extends HTMLElement {
             Esta página sincroniza con la <a target="blank" href="https://developer.clashofclans.com/#/"> API Clash of Clans</a>, lee una hoja JSON, descarga los datos y muestra con imágenes imitando la estructura real del juego, con búsqueda y selección de equipos por medio de formulario.<br>
             Datos cedidos por <a target="blank" href="https://supercell.com/en/">Supercell</a>.<br> 
             Lenguajes usados:<br>
-            <i class="fa fa-check-circle verde"></i> HTML<br>
-            <i class="fa fa-check-circle verde"></i> CSS<br>
-            <i class="fa fa-check-circle verde"></i> PHP
-        </p>
-        <p>
+            <i class="fa fa-check-circle verde"></i> HTML.<br>
+            <i class="fa fa-check-circle verde"></i> CSS.<br>
+            <i class="fa fa-check-circle verde"></i> PHP.<br><br>
             <a class="visitar" target="blank" href="https://clashofclansguerrasonline.titaniumgamers.com/">Visitar
             <i class="fas fa-arrow-alt-circle-right"></i></a>
-        </p><br>
+        </p>
     </div>
 </section>
 `;
