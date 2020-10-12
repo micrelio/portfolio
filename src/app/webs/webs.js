18e471c33ea25f9fa33872ab2593ccd38e5eb7f1
unknown <microa4@gmail.com>
2020-10-12 18:25:49 +0200

    webs cambiado a display grid

--- src/app/webs/webs.js
+++ src/app/webs/webs.js
@@ -5,29 +5,19 @@ class miWebs extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
 <section class="web">
-    <div class="titaniumGamers2">
-        <div class="logoWeb">
-            <a target="blank" href=" https://titaniumgamers.com/">
-                <img id="" class="corporativo" src="../../assets/images/logoEmpresa/electricidad.jpg">
-            </a>
-        </div>
-        <div class="titulo">
+    <div class="titaniumGamers">
+         <div class="titulo">
             <a target="blank" href=" https://titaniumgamers.com/">
                 <h1>Comunidad </h1><h1>Titanium Gamers</h1>
             </a>
         </div>
         <div class="descripcion">
-        <p>Tienda de donaciones PayPal creada durante el verano 2020 para la comunidad propia de jugadores <a target="blank" href="https://tiendasamp.titaniumgamers.com/">Titanium Gamers</a>.<br>
-        Estilos:<br>
-        <i class="fa fa-check-circle verde"></i> Bootstrap.<br>
-        <i class="fa fa-check-circle verde"></i> Display Grid.<br>
-        <i class="fa fa-check-circle verde"></i> Display Flex.<br>
-        Lenguajes:<br>
-            <i class="fa fa-check-circle verde"></i> HTML.<br>
-            <i class="fa fa-check-circle verde"></i> CSS.<br>
-            <i class="fa fa-check-circle verde"></i> Javascript.<br><br>
-            
-        </p>
+            <p>
+                Diseñada con WordPress tema DIVI (adquirida licencia desarrollador), para la comunidad propia de jugadores <a target="blank" href="https://titaniumgamers.com/">Titanium Gamers</a> durante el confinamiento 2020.<br>
+                Creada con:<br>
+                <i class="fa fa-check-circle verde"></i> WORDPRESS tema DIVI.<br><br>
+              
+            </p>
         </div>
         <div class="visitar">
             <p>
@@ -36,62 +26,57 @@ class miWebs extends HTMLElement {
             </p>
         </div>
     </div>
-
-    <div class="titanium">
-        <div class="titulo">
-            <a target="blank" href=" https://titaniumgamers.com/">
-            <h1>Comunidad</h1>
+    
+    <div class="tiendaTitanium">
+         <div class="titulo">
+            <a target="blank" href=" https://tiendasamp.titaniumgamers.com/">
+            <h1>Tienda</h1>
             <h1>Titanium Gamers</h1>
             </a>
         </div>
-        <div>
-            <p>Diseñada con WordPress tema DIVI (adquirida licencia desarrollador), para la comunidad propia de jugadores <a target="blank" href="https://titaniumgamers.com/">Titanium Gamers</a> durante el confinamiento 2020.<br>
-            Creada con:<br>
-            <i class="fa fa-check-circle verde"></i> WORDPRESS tema DIVI.<br><br>
-            <a class="visitar" target="blank" href="https://titaniumgamers.com/">Visitar
+        <div class="descripcion">
+            <p>Tienda de donaciones PayPal creada durante el verano 2020 para la comunidad propia de jugadores <a target="blank" href="https://tiendasamp.titaniumgamers.com/">Titanium Gamers</a>.<br>
+                Estilos:<br>
+                <i class="fa fa-check-circle verde"></i> Bootstrap.<br>
+                <i class="fa fa-check-circle verde"></i> Display Grid.<br>
+                <i class="fa fa-check-circle verde"></i> Display Flex.<br>
+                Lenguajes:<br>
+                <i class="fa fa-check-circle verde"></i> HTML.<br>
+                <i class="fa fa-check-circle verde"></i> CSS.<br>
+                <i class="fa fa-check-circle verde"></i> Javascript.<br><br>
+            </p>
+        </div>
+        <div class="visitar">
+            <p>
+                <a class="visitar" target="blank" href="https://tiendasamp.titaniumgamers.com/">Visitar
                 <i class="fas fa-arrow-alt-circle-right"></i></a>
             </p>
         </div>
     </div>
 
-    <div class="tiendaTitanium">
-        <div class="titulo">
-            <a target="blank" href="https://tiendasamp.titaniumgamers.com/">
-                <h1>Tienda</h1>
-                <h1>Titanium Gamers</h1>
-            </a>
-        </div>
-        <p>Tienda de donaciones PayPal creada durante el verano 2020 para la comunidad propia de jugadores <a target="blank" href="https://tiendasamp.titaniumgamers.com/">Titanium Gamers</a>.<br>
-        Estilos:<br>
-        <i class="fa fa-check-circle verde"></i> Bootstrap.<br>
-        <i class="fa fa-check-circle verde"></i> Display Grid.<br>
-        <i class="fa fa-check-circle verde"></i> Display Flex.<br>
-        Lenguajes:<br>
-            <i class="fa fa-check-circle verde"></i> HTML.<br>
-            <i class="fa fa-check-circle verde"></i> CSS.<br>
-            <i class="fa fa-check-circle verde"></i> Javascript.<br><br>
-            <a class="visitar" target="blank" href="https://tiendasamp.titaniumgamers.com/">Visitar
-            <i class="fas fa-arrow-alt-circle-right"></i></a>
-        </p>
-    </div>
-    
     <div class="primeroscoc">
-        <div class="titulo">
+         <div class="titulo">
             <a target="blank" href="https://primeroscoc.titaniumgamers.com/">
-                <h1>Primeros</h1>
-                <h1>Clash of Clans</h1>
+            <h1>Primeros</h1>
+            <h1>Clash of Clans</h1>
             </a>
         </div>
-        <p>
-            Diseñada como proyecto fin de curso ASIR durante el curso 2017-2018, a la comunidad de gamers <a target="blank" href="https://primeroscoc.titaniumgamers.com/">&#xf006;Primeros&#xf006;</a> jugadores de Clash of Clans.<br>
-            Esta API sincroniza con el servidor y muestra los datos de los equipos en tiempo real.<br>
-            Lenguajes:<br>
-            <i class="fa fa-check-circle verde"></i> HTML.<br>
-            <i class="fa fa-check-circle verde"></i> CSS.<br>
-            <i class="fa fa-check-circle verde"></i> PHP.<br><br>
-            <a  class="visitar" target="blank" href="https://primeroscoc.titaniumgamers.com/">Visitar
+        <div class="descripcion">
+            <p>
+                Diseñada como proyecto fin de curso ASIR durante el curso 2017-2018, a la comunidad de gamers <a target="blank" href="https://primeroscoc.titaniumgamers.com/">&#xf006;Primeros&#xf006;</a> jugadores de Clash of Clans.<br>
+                Esta API sincroniza con el servidor y muestra los datos de los equipos en tiempo real.<br>
+                Lenguajes:<br>
+                <i class="fa fa-check-circle verde"></i> HTML.<br>
+                <i class="fa fa-check-circle verde"></i> CSS.<br>
+                <i class="fa fa-check-circle verde"></i> PHP.<br><br>
+            </p>
+        </div>
+        <div class="visitar">
+            <p>
+            <a class="visitar" target="blank" href="https://primeroscoc.titaniumgamers.com/">Visitar
             <i class="fas fa-arrow-alt-circle-right"></i></a>
-        </p>
+            </p>
+        </div>
     </div>
 
     <div class="guerrasonline">
@@ -101,16 +86,22 @@ class miWebs extends HTMLElement {
                 <h1>Clash of Clans</h1>
             </a>
         </div>
-        <p>
-            Esta página sincroniza con la <a target="blank" href="https://developer.clashofclans.com/#/"> API Clash of Clans</a>, lee un archivo JSON, descarga los datos y muestra con imágenes imitando la estructura real del juego, con búsqueda y selección de equipos por medio de formulario.<br>
-            Datos cedidos por <a target="blank" href="https://supercell.com/en/">Supercell</a>.<br> 
-            Lenguajes:<br>
-            <i class="fa fa-check-circle verde"></i> HTML.<br>
-            <i class="fa fa-check-circle verde"></i> CSS.<br>
-            <i class="fa fa-check-circle verde"></i> PHP.<br><br>
+        <div class="descripcion">
+            <p>
+                Esta página sincroniza con la <a target="blank" href="https://developer.clashofclans.com/#/"> API Clash of Clans</a>, lee un archivo JSON, descarga los datos y muestra con imágenes imitando la estructura real del juego, con búsqueda y selección de equipos por medio de formulario.<br>
+                Datos cedidos por <a target="blank" href="https://supercell.com/en/">Supercell</a>.<br> 
+                Lenguajes:<br>
+                <i class="fa fa-check-circle verde"></i> HTML.<br>
+                <i class="fa fa-check-circle verde"></i> CSS.<br>
+                <i class="fa fa-check-circle verde"></i> PHP.<br><br>
+            </p>
+        </div>
+        <div class="visitar">
+            <p>
             <a class="visitar" target="blank" href="https://clashofclansguerrasonline.titaniumgamers.com/">Visitar
             <i class="fas fa-arrow-alt-circle-right"></i></a>
-        </p>
+            </p>
+        </div>
     </div>
 </section>
 `;
