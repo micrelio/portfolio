f41dca04b23e01e3db619870c2124c7f6aa20147
unknown <microa4@gmail.com>
2020-10-17 20:08:10 +0200

    añadido boton arriba

--- src/app/inicio/inicio.js
+++ src/app/inicio/inicio.js
@@ -49,6 +49,45 @@ class miInicio extends HTMLElement {
 }
 customElements.define("mi-inicio", miInicio);
 
+
+
+
+
+$(document).ready(function(){
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function navegar(id) {
   var render = id;
   switch (render) {
