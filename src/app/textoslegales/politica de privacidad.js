91d96e810e420d531b8659b3ebcbdbf3e451ae31
unknown <microa4@gmail.com>
2020-11-15 16:15:36 +0100

    creada pagina de politica de privacidad

---
+++ src/app/textoslegales/politica de privacidad.js
@@ -0,0 +1,186 @@
+class miPoliticadeprivacidad extends HTMLElement {
+    constructor() {
+      super();
+    }
+    connectedCallback() {
+      this.innerHTML = `
+      <section class="primera">
+          <h1> Política de privacidad</h1>
+          <div>
+              <p>
+              
+                Política de privacidad
+                Jose Antonio Lopez Baena te informa sobre su Política de Privacidad respecto del tratamiento y protección de los datos de carácter personal de los usuarios que puedan ser recabados durante la navegación a través del sitio Web https://titaniumgamers.com.
+
+                En este sentido, Jose Antonio Lopez Baena cumple con el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo de 27 de abril de 2016 relativo a la protección de las personas físicas (RGPD).
+
+                El uso de sitio Web implica la aceptación de esta Política de Privacidad así como las condiciones incluidas en el Aviso Legal.
+
+                Identidad del responsable
+                Titular: Jose Antonio Lopez Baena.
+                NIF: 48387410
+                Domicilio: Valencia - España.
+                Correo electrónico: microa4@gmail.com
+                Sitio Web: https://titaniumgamers.com
+                Principios aplicados en el tratamiento de datos
+                En el tratamiento de tus datos personales, el Titular aplicará los siguientes principios que se ajustan a las exigencias del nuevo reglamento europeo de protección de datos (RGPD):
+
+                Principio de licitud, lealtad y transparencia: El Titular siempre requerirá el consentimiento para el tratamiento de los datos personales que puede ser para uno o varios fines específicos sobre los que el Titular informará al Usuario previamente con absoluta transparencia.
+                Principio de minimización de datos: El Titular solicitará solo los datos estrictamente necesarios para el fin o los fines que los solicita.
+                Principio de limitación del plazo de conservación: El Titular mantendrá los datos personales recabados durante el tiempo estrictamente necesario para el fin o los fines del tratamiento. El Titular informará al Usuario del plazo de conservación correspondiente según la finalidad.
+                En el caso de suscripciones, el Titular revisará periódicamente las listas y eliminará aquellos registros inactivos durante un tiempo considerable.
+                Principio de integridad y confidencialidad: Los datos personales recabados serán tratados de tal manera que su seguridad, confidencialidad e integridad esté garantizada.
+                El Titular toma las precauciones necesarias para evitar el acceso no autorizado o uso indebido de los datos de sus usuarios por parte de terceros.
+                Obtención de datos personales
+                Para navegar por https://titaniumgamers.com no es necesario que facilites ningún dato personal.
+
+                Los casos en los que sí proporcionas tus datos personales son los siguientes:
+
+                Al contactar a través de los formularios de contacto o enviar un correo electrónico.
+                Derechos
+                El Titular te informa que sobre tus datos personales tienes derecho a:
+
+                Solicitar el acceso a los datos almacenados.
+                Solicitar una rectificación o la cancelación.
+                Solicitar la limitación de su tratamiento.
+                Oponerte al tratamiento.
+                Solicitar la portabilidad de tus datos.
+                El ejercicio de estos derechos es personal y por tanto debe ser ejercido directamente por el interesado, solicitándolo directamente al Titular, lo que significa que cualquier cliente, suscriptor o colaborador que haya facilitado sus datos en algún momento, puede dirigirse a Jose Antonio Lopez Baena y pedir información sobre los datos que tiene almacenados y cómo los ha obtenido, solicitar la rectificación de los mismos, solicitar la portabilidad de sus datos personales, oponerse al tratamiento, limitar su uso o solicitar la cancelación de esos datos en los ficheros del Titular.
+
+                Para ejercitar tus derechos de acceso, rectificación, cancelación, portabilidad y oposición tienes que enviar un correo electrónico a microa4@gmail.com junto con la prueba válida en derecho como una fotocopia del D.N.I. o equivalente.
+
+                Tienes derecho a la tutela judicial efectiva y a presentar una reclamación ante la autoridad de control, en este caso, la Agencia Española de Protección de Datos, si consideras que el tratamiento de datos personales que te conciernen infringe el Reglamento.
+
+                Finalidad del tratamiento de datos personales
+                Cuando te conectas al sitio Web para mandar un correo al Titular, te suscribes a su boletín estás facilitando información de carácter personal de la que el responsable es Jose Antonio Lopez Baena. Esta información puede incluir datos de carácter personal como pueden ser tu dirección IP, nombre y apellidos, dirección física, dirección de correo electrónico, número de teléfono, y otra información. Al facilitar esta información, das tu consentimiento para que tu información sea recopilada, utilizada, gestionada y almacenada por— OVH — sólo como se describe en el Aviso Legal y en la presente Política de Privacidad.
+
+                Los datos personales y la finalidad del tratamiento por parte del Titular es diferente según el sistema de captura de información:
+
+                Formularios de contacto: El Titular solicita datos personales entre los que pueden estar: nombre y apellidos, dirección de correo electrónico, número de teléfono y dirección de sitio Web con la finalidad de responder las consultas de los Usuarios.
+                Por ejemplo, Jose Antonio Lopez Baena utiliza esos datos para dar respuesta a mensajes, dudas, quejas, comentarios o inquietudes que pueden tener los Usuarios relativas a la información incluida en el sitio Web, el tratamiento de los datos personales, cuestiones referentes a los textos legales incluidos en el sitio Web, así como cualquier otra consulta que el Usuario pueda tener y que no esté sujeta a las condiciones del sitio Web.
+                Existen otras finalidades por las que el Titular trata datos personales:
+
+                Para garantizar el cumplimiento de las condiciones recogidas en el Aviso Legal y en la ley aplicable. Esto puede incluir el desarrollo de herramientas y algoritmos que ayuden a este sitio Web a garantizar la confidencialidad de los datos personales que recoge.
+                Para apoyar y mejorar los servicios que ofrece este sitio Web.
+                Para analizar la navegación de los usuarios. El Titular recoge otros datos no identificativos que se obtienen mediante el uso de cookies que se descargan en el ordenador del Usuario cuando navega por el sitio Web cuyas características y finalidad están detalladas en la Política de Cookies .
+                Para gestionar las redes sociales. Jose Antonio Lopez Baena tiene presencia en redes sociales. Si te haces seguidor en las redes sociales del Titular el tratamiento de los datos personales se regirá por este apartado, así como por aquellas condiciones de uso, políticas de privacidad y normativas de acceso que pertenezcan a la red social que proceda en cada caso y que has aceptado previamente.
+                Puedes consultar las políticas de privacidad de las principales redes sociales en estos enlaces:
+
+                Facebook
+                Twitter
+                Linkedin
+                YouTube
+                Instagram
+                El Titular tratará tus datos personales con la finalidad de administrar correctamente su presencia en la red social, informarte de sus actividades, así como para cualquier otra finalidad que las normativas de las redes sociales permitan.
+
+                En ningún caso el Titular utilizará los perfiles de seguidores en redes sociales para enviar publicidad de manera individual.
+
+                Seguridad de los datos personales
+                Para proteger tus datos personales, el Titular toma todas las precauciones razonables y sigue las mejores prácticas de la industria para evitar su pérdida, mal uso, acceso indebido, divulgación, alteración o destrucción de los mismos.
+
+                El sitio Web está alojado en OVH. La seguridad de los datos está garantizada, ya que toman todas las medidas de seguridad necesarias para ello. Puedes consultar su política de privacidad para tener más información.
+
+                Contenido de otros sitios web
+                Las páginas de este sitio Web pueden incluir contenido incrustado (por ejemplo, vídeos, imágenes, artículos, etc.). El contenido incrustado de otras web se comporta exactamente de la misma manera que si hubieras visitado la otra web.
+
+                Estos sitios Web pueden recopilar datos sobre ti, utilizar cookies, incrustar un código de seguimiento adicional de terceros, y supervisar tu interacción usando este código.
+
+                Política de Cookies
+                Para que este sitio Web funcione correctamente necesita utilizar cookies, que es una información que se almacena en tu navegador web.
+
+                En la página Política de Cookies puedes consultar toda la información relativa a la política de recogida, la finalidad y el tratamiento de las cookies.
+
+                Legitimación para el tratamiento de datos
+                La base legal para el tratamiento de tus datos es: el consentimiento.
+
+                Para contactar con el Titular, suscribirte a un boletín o realizar comentarios en este sitio Web tienes que aceptar la presente Política de Privacidad.
+
+                Categorías de datos personales
+                Las categorías de datos personales que trata el Titular son:
+
+                Datos identificativos.
+                No se tratan categorías de datos especialmente protegidos.
+                Conservación de datos personales
+                Los datos personales que proporciones al Titular se conservarán hasta que solicites su supresión.
+
+                Navegación Web
+                Al navegar por https://titaniumgamers.com se pueden recoger datos no identificativos, que pueden incluir, la dirección IP, geolocalización, un registro de cómo se utilizan los servicios y sitios, hábitos de navegación y otros datos que no pueden ser utilizados para identificarte.
+
+                El sitio Web utiliza los siguientes servicios de análisis de terceros:
+
+                El Titular utiliza la información obtenida para obtener datos estadísticos, analizar tendencias, administrar el sitio, estudiar patrones de navegación y para recopilar información demográfica.
+
+                El Titular no se hace responsable del tratamiento de los datos personales que realicen las páginas web a las que puedas acceder a través de los distintos enlaces que contiene el sitio Web.
+
+                Exactitud y veracidad de los datos personales
+                Te comprometes a que los datos facilitados al Titular sean correctos, completos, exactos y vigentes, así como a mantenerlos debidamente actualizados.
+
+                Como Usuario del sitio Web eres el único responsable de la veracidad y corrección de los datos remitidos al sitio Web exonerando a Jose Antonio Lopez Baena de cualquier responsabilidad al respecto.
+
+                Aceptación y consentimiento
+                Como Usuario del sitio Web declaras haber sido informado de las condiciones sobre protección de datos de carácter personal, aceptas y consientes el tratamiento de los mismos por parte de Jose Antonio Lopez Baena en la forma y para las finalidades indicadas en esta Política de Privacidad.
+
+                Revocabilidad
+                Para ejercitar tus derechos de acceso, rectificación, cancelación, portabilidad y oposición tienes que enviar un correo electrónico a microa4@gmail.com junto con la prueba válida en derecho como una fotocopia del D.N.I. o equivalente.
+
+                El ejercicio de estos derechos no incluye ningún dato que Jose Antonio Lopez Baena esté obligado a conservar con fines administrativos, legales o de seguridad.
+
+                Cambios en la Política de Privacidad
+                El Titular se reserva el derecho a modificar la presente Política de Privacidad para adaptarla a novedades legislativas o jurisprudenciales, así como a prácticas de la industria.
+
+                Estas políticas estarán vigentes hasta que sean modificadas por otras debidamente publicadas.
+
+                Quiénes somos
+                La dirección de nuestra web es: https://titaniumgamers.com.
+
+                Qué datos personales recogemos y por qué los recogemos
+                Comentarios
+                Cuando los visitantes dejan comentarios en la web, recopilamos los datos que se muestran en el formulario de comentarios, así como la dirección IP del visitante y la cadena de agentes de usuario del navegador para ayudar a la detección de spam.
+
+                Una cadena anónima creada a partir de tu dirección de correo electrónico (también llamada hash) puede ser proporcionada al servicio de Gravatar para ver si la estás usando. La política de privacidad del servicio Gravatar está disponible aquí: https://automattic.com/privacy/. Después de la aprobación de tu comentario, la imagen de tu perfil es visible para el público en el contexto de tu comentario.
+
+                Medios
+                Si subes imágenes a la web, deberías evitar subir imágenes con datos de ubicación (GPS EXIF) incluidos. Los visitantes de la web pueden descargar y extraer cualquier dato de ubicación de las imágenes de la web.
+
+                Formularios de contacto
+                Cookies
+                Si dejas un comentario en nuestro sitio puedes elegir guardar tu nombre, dirección de correo electrónico y web en cookies. Esto es para tu comodidad, para que no tengas que volver a rellenar tus datos cuando dejes otro comentario. Estas cookies tendrán una duración de un año.
+
+                Si tienes una cuenta y te conectas a este sitio, instalaremos una cookie temporal para determinar si tu navegador acepta cookies. Esta cookie no contiene datos personales y se elimina al cerrar el navegador.
+
+                Cuando accedas, también instalaremos varias cookies para guardar tu información de acceso y tus opciones de visualización de pantalla. Las cookies de acceso duran dos días, y las cookies de opciones de pantalla duran un año. Si seleccionas «Recuérdarme», tu acceso perdurará durante dos semanas. Si sales de tu cuenta, las cookies de acceso se eliminarán.
+
+                Si editas o publicas un artículo se guardará una cookie adicional en tu navegador. Esta cookie no incluye datos personales y simplemente indica el ID del artículo que acabas de editar. Caduca después de 1 día.
+
+                Contenido incrustado de otros sitios web
+                Los artículos de este sitio pueden incluir contenido incrustado (por ejemplo, vídeos, imágenes, artículos, etc.). El contenido incrustado de otras webs se comporta exactamente de la misma manera que si el visitante hubiera visitado la otra web.
+
+                Estas web pueden recopilar datos sobre ti, utilizar cookies, incrustar un seguimiento adicional de terceros, y supervisar tu interacción con ese contenido incrustado, incluido el seguimiento de tu interacción con el contenido incrustado si tienes una cuenta y estás conectado a esa web.
+
+                Analítica
+                Con quién compartimos tus datos
+                Cuánto tiempo conservamos tus datos
+                Si dejas un comentario, el comentario y sus metadatos se conservan indefinidamente. Esto es para que podamos reconocer y aprobar comentarios sucesivos automáticamente, en lugar de mantenerlos en una cola de moderación.
+
+                De los usuarios que se registran en nuestra web (si los hay), también almacenamos la información personal que proporcionan en su perfil de usuario. Todos los usuarios pueden ver, editar o eliminar su información personal en cualquier momento (excepto que no pueden cambiar su nombre de usuario). Los administradores de la web también pueden ver y editar esa información.
+
+                Qué derechos tienes sobre tus datos
+                Si tienes una cuenta o has dejado comentarios en esta web, puedes solicitar recibir un archivo de exportación de los datos personales que tenemos sobre ti, incluyendo cualquier dato que nos hayas proporcionado. También puedes solicitar que eliminemos cualquier dato personal que tengamos sobre ti. Esto no incluye ningún dato que estemos obligados a conservar con fines administrativos, legales o de seguridad.
+
+                Dónde enviamos tus datos
+                Los comentarios de los visitantes puede que los revise un servicio de detección automática de spam.
+
+                Tu información de contacto
+                Información adicional
+                Cómo protegemos tus datos
+                Qué procedimientos utilizamos contra las brechas de datos
+                De qué terceros recibimos datos
+                Qué tipo de toma de decisiones automatizada y/o perfilado hacemos con los datos del usuario
+                Requerimientos regulatorios de revelación de información del sector   
+              </p>
+          </div>
+      </section>
+    `;
+    }
+  }
+  customElements.define("mi-politicadeprivacidad", miPoliticadeprivacidad);
