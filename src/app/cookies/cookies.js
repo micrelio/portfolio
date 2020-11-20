1434c9360e0b62e34d816c652c93cefe30913f0f
unknown <microa4@gmail.com>
2020-11-20 18:20:22 +0100

    quitar console.logs, y formateo

--- src/app/cookies/cookies.js
+++ src/app/cookies/cookies.js
@@ -30,26 +30,13 @@ class miCookies extends HTMLElement {
       cajacookies.style.display = 'none';
     }
   }
-
-  
-
-
-
- 
-
     }
-
-
-  
-    
 }
  /* aquí guardamos la variable de que se ha
   aceptado el uso de cookies así no mostraremos
   el mensaje de nuevo */
   function aceptarCookies() {
-    console.log('aceptarCookies')
   localStorage.aceptaCookies = 'true';
   cajacookies.style.display = 'none';
 }
 customElements.define('mi-cookies', miCookies);
-
