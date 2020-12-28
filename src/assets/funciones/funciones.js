64b15129480936d3321b64bb14e9556475d9142d
unknown <microa4@gmail.com>
2020-12-28 19:05:52 +0100

    funcion fiesta funcionando en auto

--- src/assets/funciones/funciones.js
+++ src/assets/funciones/funciones.js
@@ -16,15 +16,48 @@
 //     slides[slideIndex - 1].style.display = "block";
 //     setTimeout(showSlides, 2000);
 // };
-
-function stilosFiesta(id) {
-  console.log(id);
-  // $archivo="/src/assets/style/css/styles/navidad.css"
-  switch (id) {
+function fecha(id) {
+  //   getDate(): devuelve el día del mes (1 a 31).
+  // getDay(): devuelve el número del día de la semana: desde 0 (domingo) hasta 6 (sábado).
+  // getFullYear(): cuatro dígitos del año.
+  // getHours(): hora en formato 24h (0 a 23).
+  // getMilliseconds(): milisegundos de la fecha especificada (0 a 999).
+  // getMinutes(): devuelve los minutos (0 a 59).
+  // getMonth(): devuelve el número del mes: desde 0 (enero) hasta 11 (diciembre).
+  // getSeconds(): devuelve el número de segundos (0 a 59).
+  // toLocaleDateString(): devuelve una cadena con la fecha completa, en formato de fecha local.
+  // toLocaleTimeString(): devuelve una cadena con la hora completa, en formato de hora local.
+  var objFecha = new Date();
+  var dia = objFecha.getDate();
+  var mes = 1 + objFecha.getMonth();
+  var anio = objFecha.getFullYear();
+  var festivo = "";
+  var total = mes + "" + dia;
+  if (id === "stop") {
+    festivo = "stop";
+  } else if (total > "0100" && total < "0107") {
+    festivo = "reyes";
+  } else if (total > "1200" && total < "1228") {
+    festivo = "navidad";
+  } else if (total === "1228") {
+    festivo = "inocente";
+  } else if (total > "1228") {
+    festivo = "anoNuevo";
+  }
+  switch (festivo) {
     case "stop":
       document.getElementById("stop").style = "display:none";
       document.getElementById("cssArchivo").href = "";
       document.getElementById("adorno").src = "";
+      document.getElementById("anio").innerHTML = "";
+      document.getElementById("feliz").innerHTML = "";
+      break;
+    case "reyes":
+      document.getElementById("cssArchivo").href =
+        "/src/assets/style/css/styles/styleReyes.css";
+      document.getElementById("adorno").src =
+        "/src/assets/images/gifs/reyes.gif";
+      document.getElementById("stop").style = "display:block";
       break;
     case "navidad":
       document.getElementById("cssArchivo").href =
@@ -32,6 +65,18 @@ function stilosFiesta(id) {
       document.getElementById("adorno").src =
         "/src/assets/images/gifs/noelbailando.gif";
       document.getElementById("stop").style = "display:block";
+      var feliz = "FELIZ NAVIDAD";
+      document.getElementById("feliz").innerHTML = feliz;
+      break;
+    case "inocente":
+      document.getElementById("cssArchivo").href =
+        "/src/assets/style/css/styles/styleAnoNuevo.css";
+      document.getElementById("adorno").src =
+        "/src/assets/images/gifs/inocente.gif";
+      document.getElementById("stop").style = "display:block";
+      var feliz = "FELIZ AÑO";
+      document.getElementById("feliz").innerHTML = feliz;
+      document.getElementById("anio").innerHTML = anio;
       break;
     case "anoNuevo":
       document.getElementById("cssArchivo").href =
@@ -39,54 +84,16 @@ function stilosFiesta(id) {
       document.getElementById("adorno").src =
         "/src/assets/images/gifs/brindis.gif";
       document.getElementById("stop").style = "display:block";
-      break;
-      case "reyes":
-      document.getElementById("cssArchivo").href =
-        "/src/assets/style/css/styles/styleReyes.css";
-      document.getElementById("adorno").src =
-        "/src/assets/images/gifs/reyes.gif";
-      document.getElementById("stop").style = "display:block";
+      var feliz = "FELIZ AÑO";
+      document.getElementById("feliz").innerHTML = feliz;
+      document.getElementById("anio").innerHTML = anio;
       break;
     default:
+      document.getElementById("stop").style = "display:none";
+      document.getElementById("cssArchivo").href = "";
+      document.getElementById("adorno").src = "";
+      document.getElementById("anio").innerHTML = "";
+      document.getElementById("feliz").innerHTML = "";
       break;
   }
-}
-function fecha(id) {
-  
-
-//   getDate(): devuelve el día del mes (1 a 31).
-// getDay(): devuelve el número del día de la semana: desde 0 (domingo) hasta 6 (sábado).
-// getFullYear(): cuatro dígitos del año.
-// getHours(): hora en formato 24h (0 a 23).
-// getMilliseconds(): milisegundos de la fecha especificada (0 a 999).
-// getMinutes(): devuelve los minutos (0 a 59).
-// getMonth(): devuelve el número del mes: desde 0 (enero) hasta 11 (diciembre).
-// getSeconds(): devuelve el número de segundos (0 a 59).
-// toLocaleDateString(): devuelve una cadena con la fecha completa, en formato de fecha local.
-// toLocaleTimeString(): devuelve una cadena con la hora completa, en formato de hora local.
-var objFecha = new Date();
-  var dia  = objFecha.getDate();
-  var mes  = 1 + objFecha.getMonth();
-  var anio = objFecha.getFullYear();
-var total = dia+""+mes
-console.log(total);
-
-  console.log(objFecha);
-  console.log(dia);
-  console.log(mes);
-  console.log(anio);
-switch (total) {
-  case '2812':
-    console.log('total');
-
-    break;
-    
-
-  default:
-    break;
-}
-
-
-
-
 }
