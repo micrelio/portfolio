c4dc2688d63e981f701b95c10557847ca4592929
unknown <microa4@gmail.com>
2020-12-28 17:44:23 +0100

    empezada funcion de cambio de css con fiestas

--- src/assets/funciones/funciones.js
+++ src/assets/funciones/funciones.js
@@ -51,3 +51,42 @@ function stilosFiesta(id) {
       break;
   }
 }
+function fecha(id) {
+  
+
+//   getDate(): devuelve el día del mes (1 a 31).
+// getDay(): devuelve el número del día de la semana: desde 0 (domingo) hasta 6 (sábado).
+// getFullYear(): cuatro dígitos del año.
+// getHours(): hora en formato 24h (0 a 23).
+// getMilliseconds(): milisegundos de la fecha especificada (0 a 999).
+// getMinutes(): devuelve los minutos (0 a 59).
+// getMonth(): devuelve el número del mes: desde 0 (enero) hasta 11 (diciembre).
+// getSeconds(): devuelve el número de segundos (0 a 59).
+// toLocaleDateString(): devuelve una cadena con la fecha completa, en formato de fecha local.
+// toLocaleTimeString(): devuelve una cadena con la hora completa, en formato de hora local.
+var objFecha = new Date();
+  var dia  = objFecha.getDate();
+  var mes  = 1 + objFecha.getMonth();
+  var anio = objFecha.getFullYear();
+var total = dia+""+mes
+console.log(total);
+
+  console.log(objFecha);
+  console.log(dia);
+  console.log(mes);
+  console.log(anio);
+switch (total) {
+  case '2812':
+    console.log('total');
+
+    break;
+    
+
+  default:
+    break;
+}
+
+
+
+
+}
