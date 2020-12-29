c0f2351c1a74e1ec154bd19e0ea29f2b0513626d
unknown <microa4@gmail.com>
2020-12-29 19:39:53 +0100

    acabando

--- src/assets/funciones/funciones.js
+++ src/assets/funciones/funciones.js
@@ -18,11 +18,12 @@
 // };
 function fecha(id) {
   document.getElementById("stop").style = "display:none";
-      document.getElementById("cssArchivo").href = "";
-      document.getElementById("adorno").src = "";
-      document.getElementById("anio").innerHTML = "";
-      document.getElementById("feliz").innerHTML = "";
-      document.getElementById("imagenFelicitacion").src = "";
+  document.getElementById("cssArchivo").href = "";
+  document.getElementById("adorno").src = "";
+  document.getElementById("anio").innerHTML = "";
+  document.getElementById("feliz").innerHTML = "";
+  document.getElementById("imagenFelicitacion").src = "";
+  document.getElementById("feliz").style = "color:#; font-size:#;";
   //   getDate(): devuelve el día del mes (1 a 31).
   // getDay(): devuelve el número del día de la semana: desde 0 (domingo) hasta 6 (sábado).
   // getFullYear(): cuatro dígitos del año.
@@ -41,15 +42,15 @@ function fecha(id) {
   var total = mes + "" + dia;
   if (id === "stop") {
     festivo = "stop";
-  } else if (total > "0100" && total < "0107" || id=='reyes') {
+  } else if ((total > "0100" && total < "0107") || id == "reyes") {
     festivo = "reyes";
-  } else if (total > "0200" && total < "0215" || id=="sanValentin" ) {
+  } else if ((total > "0200" && total < "0215") || id == "sanValentin") {
     festivo = "sanValentin";
-  } else if (total > "1200" && total < "1228" || id=="navidad"  ) {
+  } else if ((total > "1200" && total < "1228") || id == "navidad") {
     festivo = "navidad";
-  } else if (total === "1228" || id=="inocente" ) {
+  } else if (total === "1228" || id == "inocente") {
     festivo = "inocente";
-  } else if (total > "1228" || id=="anoNuevo" ) {
+  } else if (total > "1228" || id == "anoNuevo") {
     festivo = "anoNuevo";
   }
   // festivo='navidad'
@@ -70,7 +71,7 @@ function fecha(id) {
       document.getElementById("stop").style = "display:block";
       document.getElementById("imagenFelicitacion").src =
         "/src/assets/images/gifs/estrellaNavidad.gif";
-        document.getElementById("feliz").innerHTML = "";
+      document.getElementById("feliz").innerHTML = "";
       document.getElementById("anio").innerHTML = "";
       break;
     case "sanValentin":
@@ -81,8 +82,8 @@ function fecha(id) {
       document.getElementById("stop").style = "display:block";
       document.getElementById("imagenFelicitacion").src =
         "/src/assets/images/gifs/felizSanValentin.gif";
-        document.getElementById("feliz").innerHTML = "";
-        document.getElementById("anio").innerHTML = "";
+      document.getElementById("feliz").innerHTML = "";
+      document.getElementById("anio").innerHTML = "";
       break;
     case "navidad":
       document.getElementById("cssArchivo").href =
@@ -92,6 +93,8 @@ function fecha(id) {
       document.getElementById("stop").style = "display:block";
       var feliz = "FELIZ NAVIDAD";
       document.getElementById("feliz").innerHTML = feliz;
+      document.getElementById("feliz").style = "color:red; font-size:10rem;";
+
       document.getElementById("anio").innerHTML = anio;
       break;
     case "inocente":
