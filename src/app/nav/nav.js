8bee6971cf85f7e645c7322fe4a0f68227ddc856
unknown <microa4@gmail.com>
2020-12-31 13:19:29 +0100

    reparacion por error

--- src/app/nav/nav.js
+++ src/app/nav/nav.js
@@ -5,6 +5,7 @@ class miNav extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
   <section class="social-media">
+  <button class="selecEstilos bx bxs-brush" type="button" onclick=""></button>
 
     <div class="botonesEstilo">
       <button class="festivos" type="button" onclick="fecha('reyes');"><img src="/src/assets/images/icons/estrellaNavidad.png"></button>
@@ -13,8 +14,8 @@ class miNav extends HTMLElement {
       <button class="festivos" type="button" onclick="fecha('inocente');"> <img src="/src/assets/images/icons/inocente2.png"> </button>
       <button class="festivos" type="button" onclick="fecha('anoNuevo');"> <img src="/src/assets/images/icons/champan.png"> </button>
       
-      <button class="modo bx bx-moon" type="button" onclick="modoNoche('dia');"></button>
-      <button class="modo bx bx-sun" type="button" onclick="modoNoche('noche');"></button>
+      <button class="modo bx bxs-moon" type="button" onclick="modoNoche('dia');"></button>
+      <button class="modo bx bxs-sun" type="button" onclick="modoNoche('noche');"></button>
       
       <button class="color oro" type="button" onclick="color('oro');"></button>
       <button class="color amarillo" type="button" onclick="color('amarillo');"></button>
@@ -209,34 +210,6 @@ function nav() {
     if (w > 320 && menu.is(":hidden")) {
       menu.removeAttr("style");
     }
-  });
-}
-//----------------
-// SCRIPT DE desplegable
-function desplegable() {
-  jQuery.fn.animateAuto = function (prop, speed, callback) {
-    var elem, height, width;
-    return this.each(function (i, el) {
-      (el = jQuery(el)),
-        (elem = el
-          .clone()
-          .css({ height: "auto", width: "auto" })
-          .appendTo("body"));
-      (height = elem.css("height")), (width = elem.css("width")), elem.remove();
-      if (prop === "height") el.animate({ height: height }, speed, callback);
-      else if (prop === "width") el.animate({ width: width }, speed, callback);
-      else if (prop === "both")
-        el.animate({ width: width, height: height }, speed, callback);
-    });
-  };
-  $(window).ready(function () {
-    $("h2").click(function () {
-      if ($(this).next().hasClass("desplegado")) {
-        $(this).next().removeClass("desplegado").animate({ height: 0 }, 500);
-      } else {
-        $(this).next().addClass("desplegado").animateAuto("height", 500);
-      }
-    });
   });
 }
 //----------------
