33c3045c30bdcbdecbca497746140cc01421b835
unknown <microa4@gmail.com>
2021-01-10 20:46:18 +0100

    añadido componente para estilos

--- src/app/efecto/efecto.js
+++ src/app/efecto/efecto.js
@@ -7,15 +7,14 @@ class miEfecto extends HTMLElement {
     connectedCallback() {
         this.innerHTML =
             `
-            <h1>cargado efecto</h1>
-            <div id='c'>
+        <div id='c'>
             <div class='s'></div>
             <div class='s'></div>
             <div class='s'></div>
             <div class='s'></div>
             <div class='s'></div>
             <div class='s'></div>
-          </div>
+        </div>
      `;
     }
 }
