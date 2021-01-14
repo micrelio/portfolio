795e73e8b2cfcab8a81a07e261f4b03d0cfe8436
unknown <microa4@gmail.com>
2021-01-14 23:51:33 +0100

    no va

--- src/app/prueba/prueba.js
+++ src/app/prueba/prueba.js
@@ -1,26 +1,101 @@
-class miSaludo extends HTMLElement {
-    constructor () {
-      // Obtengo la única etiqueta 'template'
-      const tpl = document.querySelector('template');
-      // Clono su contenido y se crea una instancia del document fragment
-      const tplInst = tpl.content.cloneNode(true);
+class MiSaludo extends HTMLElement {
+     constructor () {
+        super();
+      }
       
-      super(); // invoca el constructor de la clase padre
-      // Se crea un shado dom para las instancias de mi-saludo
-      this.attachShadow({ mode: 'open'});
-      // Y se agrega el template dentro del shadow DOM usando el elemento raíz 'shadowRoot'
-      this.shadowRoot.appendChild(tplInst);
-    }
-    connectedCallback() {
-         this.innerHTML = `
-         <h1>eeeeeeeeeeeeee</h1>
-
+      connectedCallback () {
+        // let shadowRoot = this.attachShadow({mode: 'open'});
+        //  const t = document.querySelector('#sellBtn');
+        //  const instance = t.content.cloneNode(true);
+        //  shadowRoot.appendChild(instance);
+        this.innerHTML = `
+        <h1>eeeee
+        </h1>
+              
         `;
+      }
     }
-  }
+    
+    window.customElements.define('mi-saludo', MiSaludo)
+
+
+
+
+
+// let shadowRoot = this.attachShadow({mode: 'open'});
+        // const t = document.querySelector('#sellBtn');
+        // const instance = t.content.cloneNode(true);
+        // shadowRoot.appendChild(instance);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class MiSaludo extends HTMLElement {
+//     constructor () {
+//       // Obtengo la única etiqueta 'template'
+//       const tpl = document.querySelector('template');
+//       // Clono su contenido y se crea una instancia del document fragment
+//     //   const tplInst = tpl.content.cloneNode(true);
+      
+//       super(); // invoca el constructor de la clase padre
+//       //Se crea un shado dom para las instancias de mi-saludo
+//       this.attachShadow({ mode: 'open'});
+//       // Y se agrega el template dentro del shadow DOM usando el elemento raíz 'shadowRoot'
+//       this.shadowRoot.appendChild(tplInst);
+
+
+
+
+//     var template = document.getElementById('element-details-template').content;
+//       const shadowRoot = this.attachShadow({mode: 'open'}).appendChild(template.cloneNode(true));
+//     }
+// //     connectedCallback() {
+// //         this.innerHTML = `
+// //         <mi-saludo></mi-saludo>        `;
+// //   }
+//   }
   
   // Se registra el custom element para poder ser utilizado declarativamente en el HTML o imperativamente mediante JS
-  customElements.define('mi-saludo', miSaludo);
+//   customElements.define('mi-saludo', MiSaludo);
+
+
+// class MiSaludo extends HTMLElement {
+//     constructor () {
+//       // Obtengo la única etiqueta 'template'
+//       const tpl = document.querySelector('template');
+//       // Clono su contenido y se crea una instancia del document fragment
+//       const tplInst = tpl.content.cloneNode(true);
+      
+//       super(); // invoca el constructor de la clase padre
+//       // Se crea un shado dom para las instancias de mi-saludo
+//       this.attachShadow({ mode: 'open'});
+//       // Y se agrega el template dentro del shadow DOM usando el elemento raíz 'shadowRoot'
+//       this.shadowRoot.appendChild(tplInst);
+//     }
+//     connectedCallback() {
+//          this.innerHTML = `
+//          <h1>eeeeeeeeeeeeee</h1>
+
+//         `;
+//     }
+//   }
+  
+  
+//   // Se registra el custom element para poder ser utilizado declarativamente en el HTML o imperativamente mediante JS
+//   customElements.define('Mi-saludo', miSaludo);
   
 
 
@@ -29,17 +104,15 @@ class miSaludo extends HTMLElement {
 //   class extends HTMLElement {
 //     constructor() {
 //       super();
-//       var template = document
-//         .getElementById('element-details-template')
+//       var template = document.getElementById('element-details-template')
 //         .content;
-//       const shadowRoot = this.attachShadow({mode: 'open'})
-//         .appendChild(template.cloneNode(true));
+//       const shadowRoot = this.attachShadow({mode: 'open'}).appendChild(template.cloneNode(true));
 //   }
 // })
   
 
 
-{/* <element-details>
+/* <element-details>
   <span slot="element-name">slot</span>
   <span slot="description">Un marcador de posición dentro de un
      componente web que los usuarios pueden rellenar con su propio marcado,
@@ -57,14 +130,14 @@ class miSaludo extends HTMLElement {
      en el lado cliente que no se renderiza cuando la página se
      carga sino que posteriormente se puede instanciar en
      tiempo de ejecución usando JavaScript.</span>
-</element-details> */}
+</element-details> */
 
 
 
 
 //---------------------------------------------------------
 
-// class SellButton extends HTMLElement {
+// class MiSaludo extends HTMLElement {
 //       constructor () {
 //         super();
 //       }
@@ -77,7 +150,7 @@ class miSaludo extends HTMLElement {
 //       }
 //     }
     
-//     window.customElements.define('sell-button', SellButton)
+//     window.customElements.define('mi-saluddo', MiSaludo)
 
 
 //     <!-- Documento HTML con la plantilla -->
