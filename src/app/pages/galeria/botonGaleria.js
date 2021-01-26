4658b1c9cf87c07da2e31eb932840bc174768a14
unknown <microa4@gmail.com>
2021-01-26 19:25:18 +0100

    repaso css y reparaciones723

--- src/app/pages/galeria/botonGaleria.js
+++ src/app/pages/galeria/botonGaleria.js
@@ -1,80 +1,78 @@
 class miBotonera extends HTMLElement {
-    constructor() {
-        //  puedes poner argumentos
-        super();
-        //myFunction();
-    }
-    connectedCallback() {
-            this.innerHTML =
-                `
-                <style>
+  constructor() {
+    //  puedes poner argumentos
+    super();
+    //myFunction();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+    <style>
       @import "/src/app/pages/galeria/botonGaleria.css";
     </style>
-              <section id="menuGaleria" class="menuGaleria" >
-              <button class="boton" value="Galeria"  >Todas</button>
-               </section>
-              
-                  `;
-            //RECORREMOS LA SECCION ESPERANDO CUALQUIER EVENTO DEL MISMO, THIS ES EL ELEMENTO SOBRE EL QUE SE
-            //APLICA EL EVENTO, CON ESTO VEMOS QUE SE HA HECHO CLICK SOBRE UN ELEMENTO DISTINTO DEL PRINCIPAL
-            //SI HHAY SUBELEMENTOS NO VA BIEN
-            //CON ESTE OCUPAMOS MENOS RAM, YA QUE SOLO CREA UN EVENTO
-            var elementos = document.querySelector('#menuGaleria');
-            elementos.addEventListener('click', function(e) {
-                console.log(e.target.value)
-                if (e.target !== this) {
-                    // Código que se ejecuta al hacer click.
-                    console.log(e.target.value);
-                    var valor = e.target.value;
-                    //  document.getElementById("content").innerHTML = "<mi-visor></mi-visor>";
-                    console.log(valor);
-                    cargar(valor);
-                }
-            });
-            // cargaTodo();
-            setTimeout(function cx() {
-                cambio();
-            }, 0);
-            cargaBoton();
-            //cambio();
-        }
-        //   animateButton();
+    <section id="menuGaleria" class="menuGaleria" >
+        <button class="boton" value="Galeria"  >Todas</button>
+    </section>
+    `;
+    //RECORREMOS LA SECCION ESPERANDO CUALQUIER EVENTO DEL MISMO, THIS ES EL ELEMENTO SOBRE EL QUE SE
+    //APLICA EL EVENTO, CON ESTO VEMOS QUE SE HA HECHO CLICK SOBRE UN ELEMENTO DISTINTO DEL PRINCIPAL
+    //SI HHAY SUBELEMENTOS NO VA BIEN
+    //CON ESTE OCUPAMOS MENOS RAM, YA QUE SOLO CREA UN EVENTO
+    var elementos = document.querySelector("#menuGaleria");
+    elementos.addEventListener("click", function (e) {
+      console.log(e.target.value);
+      if (e.target !== this) {
+        // Código que se ejecuta al hacer click.
+        console.log(e.target.value);
+        var valor = e.target.value;
+        //  document.getElementById("content").innerHTML = "<mi-visor></mi-visor>";
+        console.log(valor);
+        cargar(valor);
+      }
+    });
+    // cargaTodo();
+    setTimeout(function cx() {
+      cambio();
+    }, 0);
+    cargaBoton();
+    //cambio();
+  }
+  //   animateButton();
 }
-customElements.define('mi-botonera', miBotonera);
+customElements.define("mi-botonera", miBotonera);
 //elemento_que_se_escuchaba.removeEventListener('evento',función_a_anular,booleano);
 function cargaBoton() {
-    //---imagenes en Galeria------------------------
-    var llaves = Object.keys(Galeria);
-    //console.log(llaves.length);
-    //---Leemos las claves
-    for (let index = 0; index < llaves.length; index++) {
-        tipo = llaves[index];
-        //  console.log('tipo', tipo);
-        //   console.log('dato', dato);
-        var boto = document.createElement('button');
-        var p = document.createElement('p');
-        //---CREAMOS UN TEXTO---
-        var t = document.createTextNode(tipo);
-        //  p.setAttribute("class", "nombre");
-        //  p.setAttribute("id", "p");
-        //  p.setAttribute("value", tipo);
-        //---CREAMOS UN BOTON---
-        boto.setAttribute("id", 'boton' + tipo);
-        boto.setAttribute("class", "boton");
-        boto.setAttribute("type", "button");
-        boto.setAttribute("value", tipo);
-        //console.log(boto)
-        // console.log(boto);
-        // console.log(p);
-        //---RENDERIZAMOS TODO
-        document.getElementById("menuGaleria").appendChild(boto);
-        //   document.getElementById('boton'+tipo).appendChild(p);
-        boto.appendChild(t);
-    }
+  //---imagenes en Galeria------------------------
+  var llaves = Object.keys(Galeria);
+  //console.log(llaves.length);
+  //---Leemos las claves
+  for (let index = 0; index < llaves.length; index++) {
+    tipo = llaves[index];
+    //  console.log('tipo', tipo);
+    //   console.log('dato', dato);
+    var boto = document.createElement("button");
+    var p = document.createElement("p");
+    //---CREAMOS UN TEXTO---
+    var t = document.createTextNode(tipo);
+    //  p.setAttribute("class", "nombre");
+    //  p.setAttribute("id", "p");
+    //  p.setAttribute("value", tipo);
+    //---CREAMOS UN BOTON---
+    boto.setAttribute("id", "boton" + tipo);
+    boto.setAttribute("class", "boton");
+    boto.setAttribute("type", "button");
+    boto.setAttribute("value", tipo);
+    //console.log(boto)
+    // console.log(boto);
+    // console.log(p);
+    //---RENDERIZAMOS TODO
+    document.getElementById("menuGaleria").appendChild(boto);
+    //   document.getElementById('boton'+tipo).appendChild(p);
+    boto.appendChild(t);
+  }
 }
 //---mirar aqui
 //    https://abalozz.es/optimiza-el-manejo-de-eventos-del-dom-en-javascript/
-//   https://es.stackoverflow.com/questions/98239/c%C3%B3mo-agregar-un-event-listener-para-varios-elementos      
+//   https://es.stackoverflow.com/questions/98239/c%C3%B3mo-agregar-un-event-listener-para-varios-elementos
 
 //---- OPCION PARA CREAR UNA ALERTA
 // var seleccion= 'boton'+ tipo;
@@ -99,6 +97,4 @@ function cargaBoton() {
 // }
 //console.log(tipo)
 //console.log(dato)
-function cambio() {
-
-};
+function cambio() {}
