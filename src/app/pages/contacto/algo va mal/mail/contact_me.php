10f3d950da06dbc0509a8a691cc86b7848da1382
unknown <microa4@gmail.com>
2021-02-16 13:07:18 +0100

    cambio de formularios

--- src/app/pages/contacto/algo va mal/mail/contact_me.php
+++ src/app/pages/contacto/algo va mal/mail/contact_me.php
@@ -20,7 +20,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'microa4@gmail.com'; //Añada su dirección de correo electrónico entre el '' reemplazando suNombre@suDominio.com - Aquí es donde el formulario enviará un mensaje. Puede ser @gmail.com
 $email_subject = "formulario web enviado por:  $name";
 $email_body = "Ha recibido un nuevo mensaje de su formulario de contacto del sitio web.\n\n"."Aquí están los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTeléfono: $phone\n\nMensaje:\n$message";
-$headers = "From: microa4@gmail.com\n";// Esta es la dirección de correo electrónico de la que se generará el mensaje. DEBE TENER EL DOMINIO DONDE ESTA ALOJADA LA WEB
+$headers = "From: info@microdeveloper.es\n";// Esta es la dirección de correo electrónico de la que se generará el mensaje. DEBE TENER EL DOMINIO DONDE ESTA ALOJADA LA WEB
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
