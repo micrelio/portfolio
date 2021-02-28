0b45b3cd996ae328641adeb32c8c5ed8f1930b21
unknown <microa4@gmail.com>
2021-02-28 18:15:37 +0100

    webs estilos punto critico

--- src/app/pages/webs/webs.js
+++ src/app/pages/webs/webs.js
@@ -26,18 +26,26 @@ class miWebs extends HTMLElement {
           <p>
             Diseñada con WordPress tema DIVI (adquirida licencia ilimitada de desarrollador), para la comunidad propia
             de jugadores <a target="blank" href="https://titaniumgamers.com/">Titanium Gamers</a> durante el
-            confinamiento 2020.<br>
-            Creada con:<br>
-            <i class="fa fa-check-circle verde"></i> WordPress tema DIVI.
-          </p>
-        </div>
+            confinamiento 2020.</p><br>
+            <p>Creada con:</p>
+            <ul>
+                <li> WordPress tema DIVI.</li>
+                
+            </ul>
+            
+           
+       
         <div class="visitar">
+       
           <p>
             <a class="visitar" target="blank" href="https://titaniumgamers.com/">Visitar
               <i class="fas fa-arrow-alt-circle-right"></i></a>
           </p>
         </div>
       </div>
+      </div>
+
+
       <div class="">
       </div>
       <div id="tiendaTitanium" class="tiendaTitanium miniWeb">
@@ -49,25 +57,32 @@ class miWebs extends HTMLElement {
         </div>
         <div class="descripcion">
           <p>Tienda de donaciones PayPal creada durante el verano 2020 para la comunidad propia de jugadores <a
-              target="blank" href="https://tiendasamp.titaniumgamers.com/">Titanium Gamers</a>.<br>
-            Estilos:<br>
-            <i class="fa fa-check-circle verde"></i> Bootstrap.<br>
-            <i class="fa fa-check-circle verde"></i> Display Grid.<br>
-            <i class="fa fa-check-circle verde"></i> Display Flex.<br>
-            Lenguajes:<br>
-            <i class="fa fa-check-circle verde"></i> HTML.<br>
-            <i class="fa fa-check-circle verde"></i> CSS.<br>
-            <i class="fa fa-check-circle verde"></i> Javascript.
-          </p>
-        </div>
+              target="blank" href="https://tiendasamp.titaniumgamers.com/">Titanium Gamers</a>.</p><br>
+              <p>Creada con: </p>
+              <ul>
+              <li>  Bootstrap.</li>
+              <li> Display Grid.</li>
+              <li> Display Flex.</li>
+              </ul>
+              <p>Lenguajes: </p>
+              <ul>
+              <li> HTML.</li>
+              <li> CSS.</li>
+              <li> Javascript.</li>
+                  
+              </ul>
+              
         <div class="visitar">
           <p>
             <a class="visitar" target="blank" href="https://tiendasamp.titaniumgamers.com/">Visitar
               <i class="fas fa-arrow-alt-circle-right"></i></a>
           </p>
         </div>
+        </div>
       </div>
       <div class="">
+
+
       </div>
       <div id="primeroscoc" class="primeroscoc miniWeb">
         <div class="titulo">
@@ -81,13 +96,14 @@ class miWebs extends HTMLElement {
             Diseñada como proyecto fin de curso ASIR durante el curso 2017-2018, a la comunidad de gamers <a
               target="blank" href="https://primeroscoc.titaniumgamers.com/">&#xf006;Primeros&#xf006;</a> jugadores de
             Clash of Clans.<br>
-            Esta API sincroniza con el servidor y muestra los datos de los equipos en tiempo real.<br>
-            Lenguajes:<br>
-            <i class="fa fa-check-circle verde"></i> HTML.<br>
-            <i class="fa fa-check-circle verde"></i> CSS.<br>
-            <i class="fa fa-check-circle verde"></i> PHP.
-          </p>
-        </div>
+            Esta API sincroniza con el servidor y muestra los datos de los equipos en tiempo real.</p><br>
+            
+            <p>Lenguajes:</p>
+              <ul>
+              <li> HTML.</li>
+              <li> CSS.</li>
+              <li> PHP.</li>
+              </ul>        
         <div class="visitar">
           <p>
             <a class="visitar" target="blank" href="https://primeroscoc.titaniumgamers.com/">Visitar
@@ -95,7 +111,9 @@ class miWebs extends HTMLElement {
           </p>
         </div>
       </div>
+      </div>
       <div class="">
+
       </div>
       <div id="guerrasOnline" class="guerrasOnline miniWeb">
         <div class="titulo">
@@ -109,19 +127,20 @@ class miWebs extends HTMLElement {
             Esta página sincroniza con la <a target="blank" href="https://developer.clashofclans.com/#/"> API Clash of
               Clans</a>, lee un archivo JSON, descarga los datos y muestra con imágenes imitando la estructura real del
             juego, con búsqueda y selección de equipos por medio de formulario.<br>
-            Datos cedidos por <a target="blank" href="https://supercell.com/en/">Supercell</a>.<br>
-            Lenguajes:<br>
-            <i class="fa fa-check-circle verde"></i> HTML.<br>
-            <i class="fa fa-check-circle verde"></i> CSS.<br>
-            <i class="fa fa-check-circle verde"></i> PHP.
-          </p>
-        </div>
+            Datos cedidos por <a target="blank" href="https://supercell.com/en/">Supercell</a>.</p><br>
+            <p>Lenguajes: </p>
+            <ul>
+            <li> HTML.</li>
+            <li> CSS.</li>
+            <li> PHP.</li>
+                   
         <div class="visitar">
           <p>
             <a class="visitar" target="blank" href="https://clashofclansguerrasonline.titaniumgamers.com/">Visitar
               <i class="fas fa-arrow-alt-circle-right"></i></a>
           </p>
         </div>
+      </div>
       </div>
       <div class="">
       </div>
